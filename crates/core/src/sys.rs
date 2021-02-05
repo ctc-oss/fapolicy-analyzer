af0a63cd2624f69d042478bf3b5194b6a79cc482
John Wass <wassj@ctc.com>
2021-02-05 16:51:44 -0500

    add bindings to trust store (#7)

--- crates/core/src/sys.rs
+++ crates/core/src/sys.rs
@@ -4,6 +4,7 @@ use fapolicyd::trust::load_ancillary_trust;
 
 pub struct SystemCfg {
     pub ancillary_trust_path: String,
+    pub system_trust_path: Option<String>,
 }
 
 #[derive(Clone)]
@@ -14,7 +15,7 @@ pub struct System {
 impl System {
     pub fn boot(cfg: SystemCfg) -> System {
         let trust: Vec<Trust> = [
-            load_system_trust(),
+            load_system_trust(&cfg.system_trust_path),
             load_ancillary_trust(cfg.ancillary_trust_path.as_str()),
         ]
         .concat();
--- py/examples/system.py
+++ py/examples/system.py
@@ -1,4 +1,10 @@
 from fapolicy_analyzer.app import System
 
-s = System("/usr/local/data2/dev/code/isis/fapolicy-analyzer/.local/fapolicyd.trust")
-s.dump_trust()
+s = System("/usr/local/data2/dev/code/isis/fapolicy-analyzer/.local/fapolicyd.trust", None)
+
+# dump the trust store
+for pt in s.trust():
+    print(f"{pt.path} {pt.size} {pt.hash}")
+
+# or
+# s.dump_trust()
