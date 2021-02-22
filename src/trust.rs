3008d40c43b875383520545fa7c405f08313c566
John Wass <wassj@ctc.com>
2021-02-22 06:58:48 -0500

    fix test

--- src/trust.rs
+++ src/trust.rs
@@ -144,6 +144,9 @@ mod tests {
         let e = parse_trust_record(s).unwrap();
         assert_eq!(e.path, "/home/user/my-ls");
         assert_eq!(e.size, 157984);
-        assert_eq!(e.hash, s.to_string());
+        assert_eq!(
+            e.hash,
+            "61a9960bf7d255a85811f4afcac51067b8f2e4c75e21cf4f2af95319d4ed1b87"
+        );
     }
 }
