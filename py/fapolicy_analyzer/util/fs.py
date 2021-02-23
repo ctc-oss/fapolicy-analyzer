d2a45113f2d7a7ccef43a96dd8eaff389bf16f95
John Wass <wassj@ctc.com>
2021-02-23 17:29:57 -0500

    add hash util

--- py/fapolicy_analyzer/util/fs.py
+++ py/fapolicy_analyzer/util/fs.py
@@ -1,5 +1,14 @@
+import hashlib
 import subprocess
 
 
 def stat(path):
     return subprocess.getoutput(f"stat {path}")
+
+
+def sha(path):
+    with open(path, "rb") as f:
+        sha256 = hashlib.sha256()
+        for b in iter(lambda: f.read(4096), b''):
+            sha256.update(b)
+        return sha256.hexdigest()
