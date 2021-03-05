cf11c75bf9037c26e8efe11b8299e8a58cc63f99
dorschs57 <dorschs57@users.noreply.github.com>
2021-03-05 11:48:51 -0500

    #33: Wired up the file choosers for the demo (#34)

--- ui/fapolicy_analyzer/ui/system_trust_database_admin.py
+++ ui/fapolicy_analyzer/ui/system_trust_database_admin.py
@@ -1,7 +1,9 @@
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
+from threading import Thread
+from time import sleep
 from fapolicy_analyzer.app import System
 from fapolicy_analyzer.util import fs
 from trust_file_list import TrustFileList
@@ -19,8 +21,9 @@ class SystemTrustDatabaseAdmin:
         self.trustFileList = TrustFileList(
             Gtk.FileChooserAction.SELECT_FOLDER, systemDb
         )
-        self.trustFileList.on_file_selection_change += (
-            self.on_trust_list_selection_change
+        self.trustFileList.on_file_selection_change += self.on_file_selection_change
+        self.trustFileList.on_database_selection_change += (
+            self.on_database_selection_change
         )
         self.builder.get_object("leftBox").pack_start(
             self.trustFileList.get_content(), True, True, 0
@@ -34,19 +37,20 @@ class SystemTrustDatabaseAdmin:
     def __build_status_markup(self, status):
         return "<b><u>T</u></b>" if status.lower() == "t" else "T"
 
+    def __get_trust(self, database):
+        sleep(1)
+        s = System(None, database, None)
+        trust = s.system_trust()
+        GLib.idle_add(self.trustFileList.set_trust, trust, self.__build_status_markup)
+
     def get_content(self):
         return self.builder.get_object("systemTrustDatabaseAdmin")
 
     def on_realize(self, *args):
-        s = System(None, self.trustFileList.get_selected_location(), None)
-        trust = s.system_trust()
-        trustStore = Gtk.ListStore(str, str, object)
-        for i, e in enumerate(trust):
-            trustStore.append([self.__build_status_markup(e.status), e.path, e])
+        if path := self.trustFileList.get_selected_location():
+            self.on_database_selection_change(path)
 
-        self.trustFileList.set_list_store(trustStore)
-
-    def on_trust_list_selection_change(self, trust):
+    def on_file_selection_change(self, trust):
         if trust:
             self.trustFileDetails.set_In_Database_View(
                 f"""File: {trust.path}
@@ -57,3 +61,8 @@ SHA256: {trust.hash}"""
                 f"""{fs.stat(trust.path)}
 SHA256: {fs.sha(trust.path)}"""
             )
+
+    def on_database_selection_change(self, database):
+        thread = Thread(target=self.__get_trust, args=(database,))
+        thread.daemon = True
+        thread.start()
