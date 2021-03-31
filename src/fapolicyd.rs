fe91fa79a952592a770125598ed1e1502134aef8
John Wass <wassj@ctc.com>
2021-03-31 17:15:06 -0400

    signal fapolicyd db update

--- src/fapolicyd.rs
+++ src/fapolicyd.rs
@@ -1,13 +1,26 @@
 // todo;; tracking the fapolicyd specific bits in here to determine if bindings are worthwhile
 
+use std::io::Write;
+
 pub const TRUST_DB_PATH: &str = "/var/lib/fapolicyd";
 pub const TRUST_FILE_PATH: &str = "/etc/fapolicyd/fapolicyd.trust";
 pub const RPM_DB_PATH: &str = "/var/lib/rpm";
+pub const FIFO_PIPE: &str = "/run/fapolicyd/fapolicyd.fifo";
 
 const USR_SHARE_ALLOWED_EXTS: [&str; 15] = [
     "pyc", "pyo", "py", "rb", "pl", "stp", "js", "jar", "m4", "php", "el", "pm", "lua", "class",
     "elc",
 ];
+
+pub fn reload_databases() {
+    std::fs::OpenOptions::new()
+        .write(true)
+        .read(false)
+        .open(FIFO_PIPE)
+        .unwrap()
+        .write("1".as_bytes())
+        .unwrap();
+}
 
 pub fn keep_entry(p: &str) -> bool {
     match p {
@@ -43,11 +56,11 @@ mod tests {
         assert!(drop_entry("/usr/include/bar.h"));
 
         // todo;; some audit results
-        // assert!(drop_entry("/usr/lib64/python3.8/LICENSE.txt"));
-        // assert!(drop_entry(
-        //     "/usr/share/bash-completion/completions/ctrlaltdel"
-        // ));
-        // assert!(drop_entry("/usr/share/zoneinfo/right/America/Santa_Isabel"));
+        assert!(drop_entry("/usr/lib64/python3.8/LICENSE.txt"));
+        assert!(drop_entry(
+            "/usr/share/bash-completion/completions/ctrlaltdel"
+        ));
+        assert!(drop_entry("/usr/share/zoneinfo/right/America/Santa_Isabel"));
 
         // all others are left in place
         assert!(keep_entry("/foo/bar"));
