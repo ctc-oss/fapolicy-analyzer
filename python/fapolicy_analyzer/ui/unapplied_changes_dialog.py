2e12123fc0d5cc18ecd04b5cb392d4bab8ae7348
Thomas Archambault <toma@son-o-wimpy.com>
2021-04-09 16:20:59 -0400

    Initial ci

---
+++ python/fapolicy_analyzer/ui/unapplied_changes_dialog.py
@@ -0,0 +1,18 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib
+from time import sleep
+from .ui_widget import UIWidget
+
+
+class UnappliedChangesDialog(UIWidget):
+    def __init__(self, parent=None, cancel_time=30):
+        super().__init__()
+        self.dialog = self.builder.get_object("unappliedChangesDialog")
+        if parent:
+            self.dialog.set_transient_for(parent)
+
+    def get_content(self):
+        return self.dialog
+
