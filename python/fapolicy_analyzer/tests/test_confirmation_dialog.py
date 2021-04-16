e0715a41b394d99a7c1392badd11f37bec892b99
Thomas Archambault <toma@son-o-wimpy.com>
2021-04-16 09:42:09 -0400

    Set longer delay

--- python/fapolicy_analyzer/tests/test_confirmation_dialog.py
+++ python/fapolicy_analyzer/tests/test_confirmation_dialog.py
@@ -22,6 +22,6 @@ def test_trust_database_admin_selection():
     dialog = ConfirmDialog("foo").get_content()
     for expected in [Gtk.ResponseType.YES, Gtk.ResponseType.NO]:
         button = dialog.get_widget_for_response(expected)
-        delayed_gui_action(button.clicked, delay=1)
+        delayed_gui_action(button.clicked, delay=5)
         response = dialog.run()
         assert response == expected
