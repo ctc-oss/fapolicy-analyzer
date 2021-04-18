45dabfc1ab3226bbe87eaa1d2cdbc02d691fecdd
John Wass <wassj@ctc.com>
2021-04-18 08:20:17 -0400

    doc

--- examples/trustdb_init.rs
+++ examples/trustdb_init.rs
@@ -7,23 +7,36 @@ use fapolicy_analyzer::cfg::All;
 use fapolicy_analyzer::rpm::load_system_trust;
 
 #[derive(Clap)]
+#[clap(name = "trustdb_init")]
+/// Initialize a fapolicyd Trust Database
+/// Default behavior is to read from the RPM database
+/// Empty database creation is possible using the `--emtpy` flag
 struct Opts {
+    /// create an empty database
     #[clap(long)]
     empty: bool,
 
+    /// Max size, in bytes, of the database backend
     #[clap(long, default_value = "104857600")]
     size: usize,
 
-    path: String,
+    /// Directory in which to create the database
+    dbdir: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
+    let path = Path::new(&opts.dbdir);
+    if !path.exists() {
+        panic!("dbdir does not exist")
+    } else if !path.is_dir() {
+        panic!("dbdir must be a directory")
+    }
 
     let env = Environment::new()
         .set_max_dbs(1)
         .set_map_size(opts.size)
-        .open(Path::new(&opts.path))
+        .open(path)
         .expect("unable to open path to trust db");
 
     let db = env
