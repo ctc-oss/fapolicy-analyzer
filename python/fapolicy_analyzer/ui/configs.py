82cf1f712775317936be61f6994c92e2cb452388
tparchambault <archambt@ctc.com>
2021-04-19 18:36:56 -0400

    Add doctests

--- python/fapolicy_analyzer/ui/configs.py
+++ python/fapolicy_analyzer/ui/configs.py
@@ -1,4 +1,10 @@
+from enum import Enum
+
 class Colors:
     LIGHT_RED = "#FF3333"
     LIGHT_YELLOW = "#FFFF33"
     LIGHT_GREEN = "light green"
+
+class StateEvents(Enum):
+    STATE_UNAPPLIED_NONE = 0
+    STATE_UNAPPLIED_CHANGES = 1
