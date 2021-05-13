b805be377fb60062a51b7cedcc43c5b2d5cd5444
John Wass <wassj@ctc.com>
2021-05-13 18:00:05 -0400

    building wheel

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 from setuptools_rust import RustExtension
 
+#
+# rm -rf fapolicy_analyzer.egg-info/ build/ dist/ && python setup.py bdist_wheel
+#
 setup(
     name="fapolicy-analyzer",
     version="0.0.4",
-    packages=["fapolicy_analyzer"] + find_namespace_packages(include=['fapolicy_analyzer.*']),
+    packages=["glade", "resources"] + find_packages(exclude=("tests",)),
     setup_requires=["setuptools", "setuptools_rust"],
     zip_safe=False,
-    rust_extensions=[RustExtension("fapolicy_analyzer.rust"),
-                     ],
+    rust_extensions=[RustExtension("fapolicy_analyzer.rust")],
+    include_package_data=True,
+    package_data={
+        "glade": ["*.glade"],
+        "resources": ["*"]
+    }
 )
