92cb495262b13767b316346c88bcf1bf16fcdbe5
John Wass <wassj@ctc.com>
2021-06-08 08:45:13 -0400

    wip

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-python setup.py bdist_wheel
+#python setup.py bdist_wheel
 
-mkdir -p "$HOME"/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+ver=$1
 
-cp dist/fapolicy_analyzer-*.whl "$HOME"/rpmbuild/SOURCES
+#rm -rf "$HOME"/rpmbuild
+#mkdir -p "$HOME"/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+
+#cp dist/fapolicy_analyzer-*.whl "$HOME"/rpmbuild/SOURCES
+#cp "$HOME/Downloads/fapolicy-analyzer-master.zip" "$HOME"/rpmbuild/SOURCES
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cp "$SCRIPT_DIR/fapolicy-analyzer.spec" "$HOME"/rpmbuild/SPECS
+cp "$SCRIPT_DIR/fapolicy-analyzer$ver.spec" "$HOME"/rpmbuild/SPECS/fapolicy-analyzer.spec
 
 rpmbuild -ba "$HOME"/rpmbuild/SPECS/fapolicy-analyzer.spec
-
+pip
