e83cfa2d5c8ab890745511a3d5c2d3dd4e848585
tparchambault <archambt@ctc.com>
2021-07-31 19:33:43 -0400

    Add unit tests to exercise __main__::main()

--- python/fapolicy_analyzer/tests/test_main.py
+++ python/fapolicy_analyzer/tests/test_main.py
@@ -1,13 +1,33 @@
 import context  # noqa: F401
 import logging
 import sys
-
+import pytest
 from unittest.mock import patch
 from ui.state_manager import stateManager
 from ui.__main__ import parse_cmdline, main
 
 
-def test_parse_args(mocker):
+@pytest.fixture
+def state():
+    yield stateManager
+
+    # Reset global objects because they persist between tests.
+    stateManager.__init__()
+    logging.root.setLevel(logging.WARNING)
+
+
+def test_parse_args_no_options(mocker, state):
+    testargs = ["prog"]
+    with patch.object(sys, 'argv', testargs):
+        parse_cmdline()
+
+        assert logging.getLogger().level == logging.WARNING
+        assert not stateManager._StateManager__bAutosaveEnabled
+        assert stateManager._StateManager__iTmpFileCount == 2
+        assert stateManager._StateManager__tmpFileBasename == "/tmp/FaCurrentSession.tmp"
+
+
+def test_parse_args_all_options(mocker, state):
     testargs = ["prog",
                 "-v",
                 "-a",
@@ -22,8 +42,23 @@ def test_parse_args(mocker):
         assert stateManager._StateManager__tmpFileBasename == "/tmp/TmpFileTemplate.tmp"
 
 
-# Currently bypassed while I work on mocking the MainWindow within main()
-def test_main(mocker):
+def test_main_no_options(mocker, state):
+    testargs = ["prog"]
+
+    with patch.object(sys, 'argv', testargs):
+        mockMW = mocker.patch("ui.__main__.MainWindow")
+        mockGtk = mocker.patch("ui.__main__.Gtk")
+        main()
+
+        assert logging.getLogger().level == logging.WARNING
+        assert not stateManager._StateManager__bAutosaveEnabled
+        assert stateManager._StateManager__iTmpFileCount == 2
+        assert stateManager._StateManager__tmpFileBasename == "/tmp/FaCurrentSession.tmp"
+        mockMW.assert_called_once()
+        mockGtk.main.assert_called_once()
+
+
+def test_main_all_options(mocker, state):
     testargs = ["prog",
                 "-v",
                 "-a",
@@ -31,12 +66,13 @@ def test_main(mocker):
                 "-c", "3"]
 
     with patch.object(sys, 'argv', testargs):
-        assert 1
-        return
-
+        mockMW = mocker.patch("ui.__main__.MainWindow")
+        mockGtk = mocker.patch("ui.__main__.Gtk")
         main()
 
-        # assert logging.getLogger().level == logging.DEBUG
-        # assert stateManager._StateManager__bAutosaveEnabled
-        # assert stateManager._StateManager__iTmpFileCount == 3
-        # assert stateManager._StateManager__tmpFileBasename == "/tmp/TmpFileTemplate.tmp"
+        assert logging.getLogger().level == logging.DEBUG
+        assert stateManager._StateManager__bAutosaveEnabled
+        assert stateManager._StateManager__iTmpFileCount == 3
+        assert stateManager._StateManager__tmpFileBasename == "/tmp/TmpFileTemplate.tmp"
+        mockMW.assert_called_once()
+        mockGtk.main.assert_called_once()
