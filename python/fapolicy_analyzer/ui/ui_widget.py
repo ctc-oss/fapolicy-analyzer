fa99f152f1f6b7034cc746b7e8280eb851917a5e
Shawn Dorsch <dorschs@ctc.com>
2021-08-04 10:03:25 -0400

    chore: Fixed loading of locales from RPM install

--- python/fapolicy_analyzer/ui/ui_widget.py
+++ python/fapolicy_analyzer/ui/ui_widget.py
@@ -1,5 +1,6 @@
 import locale
 import gi
+import pkg_resources
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -10,8 +11,8 @@ from util.format import snake_to_camelcase
 
 DOMAIN = "fapolicy_analyzer"
 locale.setlocale(locale.LC_ALL, locale.getlocale())
-with resources.path("fapolicy_analyzer", "locale") as path:
-    locale.bindtextdomain(DOMAIN, path)
+locale_path = pkg_resources.resource_filename("fapolicy_analyzer", "locale")
+locale.bindtextdomain(DOMAIN, locale_path)
 locale.textdomain(DOMAIN)
 
 
