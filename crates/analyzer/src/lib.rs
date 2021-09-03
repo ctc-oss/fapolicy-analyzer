24de85cd5f9fdc52d42018367e9d1019456b7a43
John Wass <wassj@ctc.com>
2021-09-03 13:48:51 -0400

    move rust source

--- crates/analyzer/src/lib.rs
+++ crates/analyzer/src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod api;
+pub mod app;
+pub mod cfg;
+pub mod check;
+pub mod error;
+pub mod fapolicyd;
+pub mod log;
+pub mod rpm;
+pub mod rules;
+pub mod sha;
+pub mod svc;
+pub mod sys;
+pub mod trust;
+pub mod users;
--- crates/pyo3/src/lib.rs
+++ crates/pyo3/src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use pyo3::prelude::*;
+
+pub mod acl;
+pub mod app;
+pub mod event;
+pub mod svc;
+pub mod syscheck;
+pub mod trust;
+
+#[pymodule]
+fn rust(_py: Python, m: &PyModule) -> PyResult<()> {
+    acl::init_module(_py, m)?;
+    app::init_module(_py, m)?;
+    event::init_module(_py, m)?;
+    trust::init_module(_py, m)?;
+    Ok(())
 }
