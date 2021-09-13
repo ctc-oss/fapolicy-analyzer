81ed2d4c0b51f48607be25970d75335090db285f
John Wass <wassj@ctc.com>
2021-09-13 15:08:39 -0400

    missed one

--- crates/pyo3/src/trust.rs
+++ crates/pyo3/src/trust.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
+use fapolicy_api::trust::Trust;
 use fapolicy_trust::ops::{get_path_action_map, Changeset};
 use fapolicy_trust::stat::{Actual, Status};
-use fapolicy_trust::trust::Trust;
 use pyo3::prelude::*;
 
 /// Trust entry
