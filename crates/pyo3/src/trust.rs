ff3cf3784a20f960139cc0273bc4982d864991dd
John Wass <wassj@ctc.com>
2021-10-06 07:47:22 -0400

    Bindings cleanup (#298)
    * naming conventions in bindings
    * let pyo3 roll on minor
    * /events/analysis/
    * /app/system/
    * break changes out to own mod
    * add default impl for PyChangeset
    * organize imports

--- crates/pyo3/src/trust.rs
+++ crates/pyo3/src/trust.rs
@@ -1,9 +1,6 @@
-use std::collections::HashMap;
-
 use pyo3::prelude::*;
 
 use fapolicy_api::trust::Trust;
-use fapolicy_trust::ops::{get_path_action_map, Changeset};
 use fapolicy_trust::stat::{Actual, Status};
 
 /// Trust entry
@@ -12,20 +9,20 @@ use fapolicy_trust::stat::{Actual, Status};
 #[pyclass(module = "trust", name = "Trust")]
 #[derive(Clone)]
 pub struct PyTrust {
-    pub trust: Trust,
-    pub actual: Option<Actual>,
+    pub rs_trust: Trust,
+    pub rs_actual: Option<Actual>,
     pub status: String,
 }
 impl From<Status> for PyTrust {
     fn from(status: Status) -> Self {
-        let (trust, actual, tag) = match status {
+        let (rs_trust, rs_actual, tag) = match status {
             Status::Trusted(t, act) => (t, Some(act), "T"),
             Status::Discrepancy(t, act) => (t, Some(act), "D"),
             Status::Missing(t) => (t, None, "U"),
         };
         Self {
-            trust,
-            actual,
+            rs_trust,
+            rs_actual,
             status: tag.to_string(),
         }
     }
@@ -35,24 +32,24 @@ impl From<Status> for PyTrust {
 impl PyTrust {
     #[getter]
     fn get_path(&self) -> &str {
-        &self.trust.path
+        &self.rs_trust.path
     }
 
     #[getter]
     fn get_size(&self) -> u64 {
-        self.trust.size
+        self.rs_trust.size
     }
 
     #[getter]
     fn get_hash(&self) -> &str {
-        &self.trust.hash
+        &self.rs_trust.hash
     }
 
     /// Optional actual metadata
     /// Will be None in the case of the file not existing
     #[getter]
     fn get_actual(&self) -> Option<PyActual> {
-        self.actual.as_ref().map(|a| a.clone().into())
+        self.rs_actual.as_ref().map(|a| a.clone().into())
     }
 
     #[getter]
@@ -97,57 +94,8 @@ impl PyActual {
     }
 }
 
-/// A mutable collection of changes
-#[pyclass(module = "trust", name = "Changeset")]
-#[derive(Clone)]
-pub struct PyChangeset {
-    s: Changeset,
-}
-
-impl From<Changeset> for PyChangeset {
-    fn from(s: Changeset) -> Self {
-        Self { s }
-    }
-}
-
-impl From<PyChangeset> for Changeset {
-    fn from(s: PyChangeset) -> Self {
-        s.s
-    }
-}
-
-#[pymethods]
-impl PyChangeset {
-    #[new]
-    pub fn new() -> Self {
-        Changeset::new().into()
-    }
-
-    pub fn add_trust(&mut self, path: &str) {
-        self.s.add(path)
-    }
-
-    pub fn del_trust(&mut self, path: &str) {
-        self.s.del(path)
-    }
-
-    pub fn len(&self) -> usize {
-        self.s.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-
-    pub fn get_path_action_map(&self) -> HashMap<String, String> {
-        get_path_action_map(&self.s)
-    }
-}
-
 pub fn init_module(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyTrust>()?;
     m.add_class::<PyActual>()?;
-    m.add_class::<PyChangeset>()?;
-    // m.add_wrapped(pyo3::wrap_pyfunction!(apply_trust_change))?;
     Ok(())
 }
