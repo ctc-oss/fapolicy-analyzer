62a38127a1cde7363c2fb7808c9610cf1f6a6a65
John Wass <wassj@ctc.com>
2021-10-06 07:00:14 -0400

    /app/system/

--- crates/pyo3/src/lib.rs
+++ crates/pyo3/src/lib.rs
@@ -2,13 +2,13 @@ use pyo3::prelude::*;
 
 pub mod acl;
 pub mod analysis;
-pub mod app;
+pub mod system;
 pub mod trust;
 
 #[pymodule]
 fn rust(_py: Python, m: &PyModule) -> PyResult<()> {
     acl::init_module(_py, m)?;
-    app::init_module(_py, m)?;
+    system::init_module(_py, m)?;
     analysis::init_module(_py, m)?;
     trust::init_module(_py, m)?;
     Ok(())
