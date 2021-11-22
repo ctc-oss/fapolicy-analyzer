72dae3bbf43576def3e4a685adaab01a5f2d0c53
John Wass <wassj@ctc.com>
2021-11-22 11:47:23 -0500

    docs

--- examples/service_status.py
+++ examples/service_status.py
@@ -1,13 +1,17 @@
 from fapolicy_analyzer import *
 
-# statically check status for the default fapolicyd service
+#
+# There are two ways to control a service using the dbus backend components
+#
+
+# 1. Static calls that use the default "fapolicyd" service
 print(is_fapolicyd_active())
 stop_fapolicyd()
 print(is_fapolicyd_active())
 start_fapolicyd()
 print(is_fapolicyd_active())
 
-# or control with a stateful handle to any unit by name
+# 2. Stateful object that takes a unit name
 d = Handle("fapolicyd")
 print(d.is_active)
 print(d.stop)
