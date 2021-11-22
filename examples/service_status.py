925b2680c0b589060042a20e2e7b3f14182de4ca
John Wass <wassj@ctc.com>
2021-11-22 09:42:09 -0500

    update example

--- examples/service_status.py
+++ examples/service_status.py
@@ -1,8 +1,15 @@
 from fapolicy_analyzer import *
 
-# stateful for any unit name
+# statically check status for the default fapolicyd service
+print(is_fapolicyd_active())
+stop_fapolicyd()
+print(is_fapolicyd_active())
+start_fapolicyd()
+print(is_fapolicyd_active())
+
+# or control with a stateful handle to any unit by name
 d = Handle("fapolicyd")
 print(d.is_active)
-
-# or statically for the default fapolicyd service
-print(is_fapolicyd_active())
+print(d.stop)
+print(d.is_active)
+print(d.start)
