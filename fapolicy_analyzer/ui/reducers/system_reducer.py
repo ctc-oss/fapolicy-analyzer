563b5f35230fd0593468b75357889f5870ee0555
tparchambault <development@TPArchambault.com>
2021-12-16 23:03:52 -0500

    License utility developed, 1st iteration of ctc-oss license inserted into py source

--- fapolicy_analyzer/ui/reducers/system_reducer.py
+++ fapolicy_analyzer/ui/reducers/system_reducer.py
@@ -1,3 +1,22 @@
+# CTC License # CTC License # CTC License # CTC License # CTC License # CTC
+#
+#
+# Copyright Concurrent Technologies Corporation 2021
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# CTC License # CTC License # CTC License # CTC License # CTC License # CTC
 from fapolicy_analyzer.ui.actions import SYSTEM_INITIALIZED
 from redux import Reducer, combine_reducers, handle_actions
 from .ancillary_trust_reducer import ancillary_trust_reducer
