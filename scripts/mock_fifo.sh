4b43ce7368d3d5a06992e3f20e2a75241228e168
John Wass <wassj@ctc.com>
2021-12-18 10:42:54 -0500

    Add license headers (#346)
    - Copyright and license header on all source files
    - Updates Rust code to MPLv2
    - Adds CI check to help catch new files without headers
    
    Closes #93

--- scripts/mock_fifo.sh
+++ scripts/mock_fifo.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+#
+# Copyright Concurrent Technologies Corporation 2021
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 if [[ ! -d /run/fapolicyd ]]; then mkdir /run/fapolicyd; fi
 if [[ ! -p /run/fapolicyd/fapolicyd.fifo ]]; then mkfifo /run/fapolicyd/fapolicyd.fifo; fi
--- tests/log_parse.rs
+++ tests/log_parse.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright Concurrent Technologies Corporation 2021
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use fapolicy_analyzer::events::event::Event;
 use fapolicy_analyzer::events::parse;
 use nom::combinator::map;
