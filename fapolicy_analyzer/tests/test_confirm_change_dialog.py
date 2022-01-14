51c624c8b0d3ef3b08a574142bdac6894ccd28b7
egbicker <elliotbicker@gmail.com>
2022-01-14 09:49:34 -0600

    adding tests for coverage

---
+++ fapolicy_analyzer/tests/test_confirm_change_dialog.py
@@ -0,0 +1,39 @@
+# Copyright Concurrent Technologies Corporation 2021
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from ui.confirm_change_dialog import ConfirmChangeDialog
+
+from fapolicy_analyzer.ui.strings import (
+    CONFIRM_UNTRUST_SELECTION,
+    CONFIRM_TRUST_SELECTION
+)
+
+def test_creates_widget():
+    widget = ConfirmChangeDialog(parent=Gtk.Window())
+    assert type(widget) is ConfirmChangeDialog
+
+def test_confirm_trust_dialog_message():
+    n_files = 2
+    widget = ConfirmChangeDialog(parent=Gtk.Window(), n_total=n_files, n_atdb=0)
+    buff = widget.get_object("confirmInfo").get_buffer()
+    start = buff.get_start_iter()
+    end = buff.get_end_iter()
+    dialog_string = buff.get_text(start, end, False)
+    assert_string = f"{n_files} files will be trusted. " + CONFIRM_TRUST_SELECTION
+    assert dialog_string == assert_string
