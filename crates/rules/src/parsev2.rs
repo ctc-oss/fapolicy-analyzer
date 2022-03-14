47bea21c1db79955edf8bace0f42ebb014b058fd
John Wass <wassj@ctc.com>
2022-03-14 18:19:37 -0400

    parallel subj and obj

--- crates/rules/src/parsev2.rs
+++ crates/rules/src/parsev2.rs
@@ -7,8 +7,11 @@ use nom::IResult;
 use crate::parser::error::RuleParseError;
 use crate::parser::error::RuleParseError::*;
 use crate::parser::trace::Trace;
-use crate::{Decision, Permission};
-use nom::character::complete::alphanumeric1;
+use crate::{Decision, ObjPart, Object, Permission, Rvalue, SubjPart, Subject};
+use nom::character::complete::{alphanumeric1, digit1, space0, space1};
+use nom::character::is_alphanumeric;
+use nom::error::ErrorKind;
+use nom::multi::separated_list1;
 
 type StrTrace<'a> = Trace<&'a str>;
 type TraceError<'a> = RuleParseError<StrTrace<'a>>;
@@ -75,19 +78,104 @@ pub fn permission(i: StrTrace) -> IResult<StrTrace, Permission, TraceError> {
 }
 
 #[derive(Debug)]
-pub struct SubObj<I> {
-    subject: I,
-    object: I,
+pub struct SubObj {
+    subject: Subject,
+    object: Object,
 }
 
-pub fn subject_object_parts(i: StrTrace) -> IResult<StrTrace, SubObj<StrTrace>, TraceError> {
+fn filepath(i: StrTrace) -> IResult<StrTrace, StrTrace, TraceError> {
+    nom::bytes::complete::is_not(" \t\n")(i)
+}
+
+fn pattern(i: StrTrace) -> IResult<StrTrace, StrTrace, TraceError> {
+    nom::bytes::complete::take_while1(|x| is_alphanumeric(x as u8) || x == '_')(i)
+}
+
+fn trust_flag(i: StrTrace) -> IResult<StrTrace, bool, TraceError> {
+    match digit1(i) {
+        Ok((r, v)) if v.fragment == "1" => Ok((r, true)),
+        Ok((r, v)) if v.fragment == "0" => Ok((r, false)),
+        Ok((_, v)) => Err(nom::Err::Failure(Nom(i, ErrorKind::Digit))),
+        Err(e) => Err(e),
+    }
+}
+
+fn subj_part(i: StrTrace) -> IResult<StrTrace, SubjPart, TraceError> {
+    alt((
+        map(tag("all"), |_| SubjPart::All),
+        map(
+            separated_pair(tag("uid"), tag("="), digit1),
+            |x: (StrTrace, StrTrace)| SubjPart::Uid(x.1.fragment.parse().unwrap()),
+        ),
+        map(
+            separated_pair(tag("gid"), tag("="), digit1),
+            |x: (StrTrace, StrTrace)| SubjPart::Gid(x.1.fragment.parse().unwrap()),
+        ),
+        map(
+            separated_pair(tag("exe"), tag("="), filepath),
+            |x: (StrTrace, StrTrace)| SubjPart::Exe(x.1.fragment.to_string()),
+        ),
+        map(
+            separated_pair(tag("pattern"), tag("="), pattern),
+            |x: (StrTrace, StrTrace)| SubjPart::Pattern(x.1.fragment.to_string()),
+        ),
+        map(
+            separated_pair(tag("comm"), tag("="), filepath),
+            |x: (StrTrace, StrTrace)| SubjPart::Comm(x.1.fragment.to_string()),
+        ),
+        map(
+            separated_pair(tag("trust"), tag("="), trust_flag),
+            |x: (StrTrace, bool)| SubjPart::Trust(x.1),
+        ),
+    ))(i)
+}
+
+pub fn subject(i: StrTrace) -> IResult<StrTrace, Subject, TraceError> {
+    map(separated_list1(space1, subj_part), |parts| {
+        Subject::new(parts)
+    })(i)
+}
+
+fn obj_part(i: StrTrace) -> IResult<StrTrace, ObjPart, TraceError> {
+    alt((
+        map(tag("all"), |_| ObjPart::All),
+        map(
+            separated_pair(tag("device"), tag("="), filepath),
+            |x: (StrTrace, StrTrace)| ObjPart::Device(x.1.fragment.to_string()),
+        ),
+        map(
+            separated_pair(tag("dir"), tag("="), filepath),
+            |x: (StrTrace, StrTrace)| ObjPart::Dir(x.1.fragment.to_string()),
+        ),
+        map(
+            separated_pair(tag("ftype"), tag("="), filepath),
+            |x: (StrTrace, StrTrace)| ObjPart::FileType(Rvalue::Literal(x.1.fragment.to_string())),
+        ),
+        map(
+            separated_pair(tag("path"), tag("="), filepath),
+            |x: (StrTrace, StrTrace)| ObjPart::Path(x.1.fragment.to_string()),
+        ),
+        map(
+            separated_pair(tag("trust"), tag("="), trust_flag),
+            |x: (StrTrace, bool)| ObjPart::Trust(x.1),
+        ),
+    ))(i)
+}
+
+pub fn object(i: StrTrace) -> IResult<StrTrace, Object, TraceError> {
+    map(separated_list1(space1, obj_part), |parts| {
+        Object::new(parts)
+    })(i)
+}
+
+pub fn subject_object_parts(i: StrTrace) -> IResult<StrTrace, SubObj, TraceError> {
     if !i.fragment.contains(":") {
         return Err(nom::Err::Error(MissingSeparator(i)));
     }
 
     let (x, y) = take_until(":")(i)?;
 
-    match separated_pair(opt(take_until(":")), tag(":"), opt(rest))(i) {
+    match separated_pair(opt(subject), tuple((space0, tag(":"), space0)), opt(object))(i) {
         Ok((remaining, (Some(s), Some(o)))) => Ok((
             remaining,
             SubObj {
