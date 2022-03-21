c60acb3827662e77e831bdd1a2885bb3df06fff4
John Wass <wassj@ctc.com>
2022-03-21 07:02:58 -0400

    headers

--- crates/rules/src/parser/errat.rs
+++ crates/rules/src/parser/errat.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright Concurrent Technologies Corporation 2021
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use crate::parse::StrTrace;
 use crate::parser::error::RuleParseError;
 use crate::parser::error::RuleParseError::*;
--- crates/rules/src/parser/legacy.rs
+++ crates/rules/src/parser/legacy.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright Concurrent Technologies Corporation 2021
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use crate::parse::{StrTrace, TraceError};
 use crate::{parse, Decision, Object, Permission, Subject};
 use nom::bytes::complete::{is_not, take_until};
