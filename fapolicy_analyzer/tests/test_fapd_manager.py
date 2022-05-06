4db17675ca480d8225c2b32029e4dd6cfb15f9bd
tparchambault <development@TPArchambault.com>
2022-05-05 20:40:40 -0400

    Add mock fapd handle

--- fapolicy_analyzer/tests/test_fapd_manager.py
+++ fapolicy_analyzer/tests/test_fapd_manager.py
@@ -80,8 +80,8 @@ def test_start_profiling(fapdManager, mocker):
     mockFapdHandle = MagicMock()
     fapdManager._fapd_ref = mockFapdHandle
     fapdManager._fapd_ref.is_active.side_effect = [True]
-    # mockProcess = MagicMock()
-    #  mockSubproc = mocker.patch("fapolicy_analyzer.ui.fapd_manager.subprocess.Popen", return_value=mockProcess)
+    mockProcess = MagicMock()
+    mocker.patch("fapolicy_analyzer.ui.fapd_manager.subprocess.Popen", return_value=mockProcess)
     fapdManager.mode = FapdMode.ONLINE
     fapdManager.start(FapdMode.PROFILING)
     mockFapdHandle.stop.assert_called()
