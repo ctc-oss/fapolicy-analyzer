06509819a3dfc971faded2c4e7b316efbd5eaaf9
egbicker <elliotbicker@gmail.com>
2022-05-20 12:03:29 -0500

    cleanup

--- fapolicy_analyzer/ui/policy_rules_admin_page.py
+++ fapolicy_analyzer/ui/policy_rules_admin_page.py
@@ -105,6 +105,7 @@ class PolicyRulesAdminPage(UIConnectedWidget, UIPage):
             details_widget_name="objectDetails",
         )
         object_tabs.append_page(self.object_list.get_ref(), Gtk.Label(label="Object"))
+
         self.__switchers = [
             self.Switcher(
                 self.get_object("userPanel"),
@@ -476,6 +477,7 @@ class PolicyRulesAdminPage(UIConnectedWidget, UIPage):
         last_selection = next(iter(files[-1:]), None)
         details = self.get_object(details_widget_name)
         details.get_buffer().set_text(fs.stat(last_selection) if last_selection else "")
+
         if secondary_action:
             secondary_action()
 
