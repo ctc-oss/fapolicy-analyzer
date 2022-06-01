769e24730408bbf178794246044b618b7e2cb2aa
tparchambault <development@TPArchambault.com>
2022-06-01 10:34:13 -0400

    Updated for recent API. Added CL option to specify event log

--- examples/parse_events.py
+++ examples/parse_events.py
@@ -13,29 +13,81 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import  sys
+import logging
+logging.basicConfig(level=logging.WARNING)
+import argparse
 from fapolicy_analyzer import *
 
-# config is loaded from $HOME/.config/fapolicy-analyzer/fapolicy-analyzer.toml
-s1 = System()
-print(f"found {len(s1.users())} system users")
-print(f"found {len(s1.groups())} system groups")
+# Globals
+verbose_mode = False
+fapd_debug_log="events0.log"
 
-umap = {u.id: u.name for u in s1.users()}
-gmap = {g.id: g.name for g in s1.groups()}
-#for e in s1.events_from("events0.log")[:5]:
-print(s1.load_debuglog("../tests/data/events0.log"))
-#for e in s1.load_debuglog("../tests/data/events0.log"): #[:5]:
-#    print({"u": umap[e.uid],
-#           "g": gmap[e.gid],
-#           "s": {
-#               "file": e.subject.file,
-#               "trust": e.subject.trust,
-#               "access": e.subject.access
-#           },
-#           "o": {
-#               "file": e.object.file,
-#               "trust": e.object.trust,
-#               "access": e.object.access,
-#               "mode": e.object.mode
-#           }})
-#print("...")
+def parse_cmdline():
+    global verbose_mode
+    global fapd_debug_log
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Enable verbose mode"
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Specify the fapolicyd event debug log [default: events0.log ]",
+    )
+
+    args = parser.parse_args()
+
+    # Set Verbosity Level
+    if args.verbose:
+        verbose_mode = True
+        logging.root.setLevel(logging.DEBUG)
+        logging.debug("Verbosity enabled.")
+
+    # Set input fapolicyd event log [default: events0.log]
+    if args.input:
+        fapd_debug_log=args.input
+
+def main():
+    parse_cmdline()
+    
+    # config loaded from $HOME/.config/fapolicy-analyzer/fapolicy-analyzer.toml
+    s1 = System()
+    print(f"found {len(s1.users())} system users")
+    print(f"found {len(s1.groups())} system groups")
+
+    umap = {u.id: u.name for u in s1.users()}
+    gmap = {g.id: g.name for g in s1.groups()}
+
+    # Only iterate and print maps if in verbose mode
+    if verbose_mode:
+        logging.debug("\n\nUser Map:")
+        for u in umap:
+            logging.debug(f"{u}:\t{umap[u]}")
+
+        logging.debug("\n\nGroup Map:")
+        for g in gmap:
+            logging.debug(f"{g}:\t{gmap[g]}")
+
+    debug_log = s1.load_debuglog(fapd_debug_log)
+    for s in debug_log.subjects():
+        logging.debug(f" - Getting all events associated with subject: {s}")
+        for e in debug_log.by_subject(s):
+            print({"u": umap[e.uid],
+                   "g": gmap[e.gid], 
+                   "s": {
+                       "file": e.subject.file,
+                       "trust": e.subject.trust,
+                       "access": e.subject.access
+                   },
+                   "o": {
+                       "file": e.object.file,
+                       "trust": e.object.trust,
+                       "access": e.object.access,
+                       "mode": e.object.mode
+                   }})
+        print("...")
+
+if __name__ == "__main__":
+    main()
