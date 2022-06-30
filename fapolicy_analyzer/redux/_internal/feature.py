0f516302d95f06250400e9ef2510feed2a1c480c
John Wass <wassj@ctc.com>
2022-06-30 15:26:22 -0400

    headers

--- fapolicy_analyzer/redux/_internal/feature.py
+++ fapolicy_analyzer/redux/_internal/feature.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Dr. Carsten Leue
+# Copyright Concurrent Technologies Corporation 2021
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
