2a25acfb704f650282c157aec1a1dd9ef79dfba5
John Wass <wassj@ctc.com>
2022-07-11 08:07:57 -0400

    debug test

--- crates/rules/tests/write.rs
+++ crates/rules/tests/write.rs
@@ -90,7 +90,7 @@ fn test_dir_multi_file() -> Result<(), Box<dyn Error>> {
 
     let expected = vec![expected0, expected1];
     for (i, f) in read_dir(rules_d)?.enumerate() {
-        let actual = read_string(&f.unwrap().path())?.trim();
+        let actual = read_string(&f.unwrap().path())?.trim().to_string();
         println!("{}: expected [{}], actual[{}]", i, expected[i], actual);
         assert_eq!(expected[i], actual);
     }
@@ -124,7 +124,7 @@ fn test_dir_multi_file_multi_rule() -> Result<(), Box<dyn Error>> {
     let concat = format!("{}\n{}", expected0, expected1);
     let expected = vec![concat.as_str(), expected2];
     for (i, f) in read_dir(rules_d)?.enumerate() {
-        let actual = read_string(&f.unwrap().path())?.trim();
+        let actual = read_string(&f.unwrap().path())?.trim().to_string();
         println!("{}: expected [{}], actual[{}]", i, expected[i], actual);
         assert_eq!(expected[i], actual);
     }
