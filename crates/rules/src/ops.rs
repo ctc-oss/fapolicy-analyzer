2052e0d975571d30e7b7470e734e9029bd1e2d0f
John Wass <wassj@ctc.com>
2022-07-13 11:21:33 -0400

    Refine Rule Change API (#535)
    Several refinements and additions to the rule change API:
    
    - Adds a `parse(str)` method to the RuleChangeset to expose marker parsing errors
    - Adds a new accessor for clarity, `rules()` to replace `get()`.
        - `get()` should be considered deprecated
    - Retains the text from the previous successful parse and provides accessor `text()`
    - Updates the `change_rules.py` example
    
    #495

--- crates/rules/src/ops.rs
+++ crates/rules/src/ops.rs
@@ -8,12 +8,14 @@
 
 use crate::db::{RuleEntry, DB};
 
+use crate::error::Error;
 use crate::read::deserialize_rules_db;
 
 // Mutable
 #[derive(Default, Clone, Debug)]
 pub struct Changeset {
     db: DB,
+    src: Option<String>,
 }
 
 impl Changeset {
@@ -21,9 +23,13 @@ impl Changeset {
         &self.db
     }
 
+    pub fn src(&self) -> Option<&String> {
+        self.src.as_ref()
+    }
+
     // todo;; how to properly convey lints and errors in the parse fail?
     //        perhaps just roll it up to a _simple_ Error/Warn/Ok result enum
-    pub fn set(&mut self, text: &str) -> Result<&DB, String> {
+    pub fn set(&mut self, text: &str) -> Result<&DB, Error> {
         // todo;; what to do with the source text here?
         //        writing it out verbatim to the disk at deploy would be ideal
         //        but it has to be stashed somewhere until writing at deploy time
@@ -33,9 +39,10 @@ impl Changeset {
         match deserialize_rules_db(text) {
             Ok(r) => {
                 self.db = r;
+                self.src = Some(text.to_string());
                 Ok(&self.db)
             }
-            Err(_) => Err("failed to deserialize db".to_string()),
+            Err(e) => Err(e),
         }
     }
 
