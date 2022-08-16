300c83ddb4726051007a90fa2a8bbfa59f7bd1b9
John Wass <wassj@ctc.com>
2022-08-16 19:25:42 -0400

    accept hyphen args for target

--- crates/tools/fapolicy_profiler.rs
+++ crates/tools/fapolicy_profiler.rs
@@ -16,6 +16,7 @@
 use clap::Clap;
 use fapolicy_daemon::profiler::Profiler;
 use fapolicy_rules::read::load_rules_db;
+use nom::combinator::opt;
 use std::error::Error;
 use std::process::Command;
 
@@ -23,22 +24,28 @@ use std::process::Command;
 #[clap(name = "File Access Policy Profiler", version = "v0.0.0")]
 struct Opts {
     /// path to *.rules or rules.d
-    rules_path: Option<String>,
+    #[clap(short, long)]
+    rules: Option<String>,
+
+    #[clap(allow_hyphen_values = true)]
+    target: Vec<String>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opts: Opts = Opts::parse();
+    eprintln!("profiling: {:?}", opts.target);
+    let target = opts.target.first().expect("target not specified");
+    let args: Vec<&String> = opts.target.iter().skip(1).collect();
+
     let mut profiler = Profiler::new();
-    let db = if let Some(rules_path) = opts.rules_path {
+    let db = if let Some(rules_path) = opts.rules {
         Some(load_rules_db(&rules_path)?)
     } else {
         None
     };
 
     profiler.activate_with_rules(db.as_ref())?;
-    let out = Command::new("cat")
-        .arg("/etc/fapolicyd/compiled.rules")
-        .output()?;
+    let out = Command::new(target).args(args).output()?;
     println!("{}", String::from_utf8(out.stdout)?);
     profiler.deactivate()?;
 
