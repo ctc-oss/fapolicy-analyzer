9f400e57252dc58aaf494a1de040a4b7dcd76466
Shawn Dorsch <dorschs@ctc.com>
2022-09-02 11:50:41 -0400

    Updated output refresh to happen with a dispatch

--- fapolicy_analyzer/ui/profiler_page.py
+++ fapolicy_analyzer/ui/profiler_page.py
@@ -14,14 +14,14 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-from events import Events
 from time import sleep
 
 import gi
+from events import Events
 from fapolicy_analyzer.ui.actions import (
-    set_profiler_state,
-    set_profiler_output,
     clear_profiler_state,
+    set_profiler_output,
+    set_profiler_state,
 )
 from fapolicy_analyzer.ui.faprofiler import FaProfiler
 from fapolicy_analyzer.ui.store import dispatch, get_system_feature
@@ -34,7 +34,9 @@ from gi.repository import Gtk  # isort: skip
 
 class ProfilerPage(UIConnectedWidget, UIPage, Events):
     def __init__(self, fapd_manager):
-        UIConnectedWidget.__init__(self, get_system_feature(), on_next=self.on_next_system)
+        UIConnectedWidget.__init__(
+            self, get_system_feature(), on_next=self.on_next_system
+        )
         self.__events__ = [
             "analyze_button_pushed",
             "refresh_toolbar",
@@ -92,10 +94,11 @@ class ProfilerPage(UIConnectedWidget, UIPage, Events):
             self.update_field_text(system.get("profiler").entry)
 
         buff = self.get_object("profilerOutput").get_buffer()
-        text = buff.get_text(buff.get_start_iter(), buff. get_end_iter(), True)
+        text = buff.get_text(buff.get_start_iter(), buff.get_end_iter(), True)
         if not text == system.get("profiler").output:
             self.update_output_text(system.get("profiler").output)
-        self.analysis_available = bool(system.get("profiler").output)
+            self.analysis_available = bool(system.get("profiler").output)
+            self.refresh_toolbar()
 
     def update_field_text(self, profilerDict):
         for k, v in profilerDict.items():
@@ -129,28 +132,27 @@ class ProfilerPage(UIConnectedWidget, UIPage, Events):
         return self.inputDict
 
     def display_log_output(self):
-        text_display = self.get_object("profilerOutput")
-        buff = Gtk.TextBuffer()
-        files = [self._fapd_profiler.fapd_prof_stderr,
-                 self._fapd_profiler.fapd_prof_stdout,
-                 self._fapd_profiler.faprofSession.tgtStderr,
-                 self._fapd_profiler.faprofSession.tgtStdout]
+        text = ""
+        files = [
+            self._fapd_profiler.fapd_prof_stderr,
+            self._fapd_profiler.fapd_prof_stdout,
+            self._fapd_profiler.faprofSession.tgtStderr,
+            self._fapd_profiler.faprofSession.tgtStdout,
+        ]
 
         for run_file in files:
-            buff.insert_markup(buff.get_end_iter(), f"<b>{run_file}</b>\n", -1)
+            text += f"<b>{run_file}</b>\n"
             try:
                 spacers = 10
                 if run_file is not None:
                     with open(run_file, "r") as f:
                         lines = f.readlines()
-                    buff.insert(buff.get_end_iter(), "".join(lines + ["\n"]))
+                    text += "".join(lines + ["\n"])
                     spacers = len(run_file)
-                buff.insert_markup(buff.get_end_iter(), "<b>" + "=" * spacers + "</b>\n", -1)
+                text += "<b>" + "=" * spacers + "</b>\n"
             except OSError as ex:
                 logging.error(f"There was an issue reading from {run_file}.", ex)
 
-        text_display.set_buffer(buff)
-        text = buff.get_text(buff.get_start_iter(), buff. get_end_iter(), True)
         dispatch(set_profiler_output(text))
 
     def on_test_activate(self, *args):
@@ -168,4 +170,3 @@ class ProfilerPage(UIConnectedWidget, UIPage, Events):
         self._fapd_profiler.stop_prof_session()
         self.running = False
         self.display_log_output()
-        self.refresh_toolbar()
