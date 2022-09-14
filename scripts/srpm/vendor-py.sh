6b801159b43c6992535828dc1cf1c0d31c4f7376
John Wass <wassj@ctc.com>
2022-09-14 14:17:09 -0400

    only vendor py when needed

--- scripts/srpm/vendor-py.sh
+++ scripts/srpm/vendor-py.sh
@@ -15,17 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# rust
-rm -rf vendor-rs
-cargo vendor-filterer --platform=x86_64-unknown-linux-gnu vendor-rs &> /dev/null
-./scripts/srpm/lock2spec.py
-mkdir vendor-tmp
-mv vendor-rs vendor-tmp/vendor
-mv vendor-tmp vendor-rs
-tar czf vendor-rs.tar.gz -C vendor-rs .
+rm -rf vendor-py && mkdir vendor-py
+cd vendor-py
 
-# python
-rm -rf vendor-py && mkdir vendor-py && cd vendor-py
 curl -sLO https://github.com/pypa/pip/archive/refs/tags/21.3.1.tar.gz
 curl -sLO https://github.com/pypa/setuptools/archive/refs/tags/v59.6.0.tar.gz
 curl -sLO https://github.com/rbarrois/python-semanticversion/archive/refs/tags/2.8.2.tar.gz
@@ -35,6 +27,4 @@ git clone https://github.com/PyO3/setuptools-rust.git -b v1.1.2 &> /dev/null
 
 cd ..
 tar czf vendor-py.tar.gz -C vendor-py .
-
-du -sh vendor-rs.tar.gz
 du -sh vendor-py.tar.gz
