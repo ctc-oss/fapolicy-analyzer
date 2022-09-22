f4c4c201dde7ffbf6b5b230a23c2079d05ae7fea
egbicker <51774793+egbicker@users.noreply.github.com>
2022-09-22 10:18:57 -0400

    Fix revisiting analysis from profiler (#648)
    Fixes an issue with accessing the analysis view a second time after profiling.
    
    Closes #639

--- fapolicy_analyzer/tests/reducers/test_profiler_reducer.py
+++ fapolicy_analyzer/tests/reducers/test_profiler_reducer.py
@@ -27,12 +27,12 @@ from fapolicy_analyzer.ui.reducers.profiler_reducer import (
 def test_handle_set_profiler_state():
     new_state = {"key1": "value1"}
     result = handle_set_profiler_state(
-        ProfilerState(entry={"oldkey": "oldvalue"}, output=""),
+        ProfilerState(entry={"oldkey": "oldvalue"}, output="", file=""),
         MagicMock(payload=new_state)
     )
-    assert result == ProfilerState(entry=new_state, output="")
+    assert result == ProfilerState(entry=new_state, output="", file="")
 
 
 def test_handle_clear_profiler_state():
-    result = handle_clear_profiler_state(ProfilerState(entry={"oldkey": "oldvalue"}, output=""))
-    assert result == ProfilerState(entry=default_entry, output="")
+    result = handle_clear_profiler_state(ProfilerState(entry={"oldkey": "oldvalue"}, output="", file=""))
+    assert result == ProfilerState(entry=default_entry, output="", file="")
