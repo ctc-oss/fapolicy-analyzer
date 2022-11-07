8e91e179ed63cecd5ff5883af29f8b59f7b63ecf
John Wass <wassj@ctc.com>
2022-11-07 13:02:49 -0500

    Temporal filtering of events (#662)
    Adds begin and until setters to the Python API EventLog type allowing filtering of events using these temporal bounds.
    
    Supports #465

--- crates/pyo3/src/analysis.rs
+++ crates/pyo3/src/analysis.rs
@@ -82,6 +82,10 @@ impl PyEvent {
     fn rule_id(&self) -> i32 {
         self.rs.event.rule_id
     }
+
+    fn when(&self) -> Option<i64> {
+        self.rs.event.when.map(|t| t.timestamp())
+    }
 }
 
 /// Subject metadata
@@ -185,6 +189,28 @@ impl PyObject {
 pub struct PyEventLog {
     pub(crate) rs: EventDB,
     pub(crate) rs_trust: TrustDB,
+    start: Option<i64>,
+    stop: Option<i64>,
+}
+
+impl PyEventLog {
+    pub(crate) fn new(rs: EventDB, trust: TrustDB) -> Self {
+        Self {
+            rs,
+            rs_trust: trust,
+            start: None,
+            stop: None,
+        }
+    }
+
+    fn temporal_filter(&self, e: &PyEvent) -> bool {
+        match (e.rs.event.when, self.start, self.stop) {
+            (None, _, _) | (_, None, None) => true,
+            (Some(t), Some(begin), None) => t.timestamp() >= begin,
+            (Some(t), None, Some(end)) => t.timestamp() <= end,
+            (Some(t), Some(begin), Some(end)) => t.timestamp() >= begin && t.timestamp() <= end,
+        }
+    }
 }
 
 #[pymethods]
@@ -195,12 +221,25 @@ impl PyEventLog {
         m.into_iter().collect()
     }
 
+    fn begin(&mut self, start: Option<i64>) {
+        self.start = start;
+    }
+
+    fn until(&mut self, stop: Option<i64>) {
+        self.stop = stop;
+    }
+
     /// Get events that fit the given subject perspective perspective
-    fn by_subject(&self, path: String) -> Vec<PyEvent> {
-        analyze(&self.rs, Perspective::Subject(path), &self.rs_trust)
-            .iter()
-            .flat_map(|e| expand_on_gid(e))
-            .collect()
+    fn by_subject(&self, path: &str) -> Vec<PyEvent> {
+        analyze(
+            &self.rs,
+            Perspective::Subject(path.to_string()),
+            &self.rs_trust,
+        )
+        .iter()
+        .flat_map(expand_on_gid)
+        .filter(|e| self.temporal_filter(e))
+        .collect()
     }
 
     /// Get events that fit the given user perspective
@@ -208,6 +247,7 @@ impl PyEventLog {
         analyze(&self.rs, Perspective::User(uid), &self.rs_trust)
             .iter()
             .flat_map(|e| expand_on_gid(e).into_iter().filter(|e| e.uid() == uid))
+            .filter(|e| self.temporal_filter(e))
             .collect()
     }
 
@@ -216,7 +256,74 @@ impl PyEventLog {
         analyze(&self.rs, Perspective::Group(gid), &self.rs_trust)
             .iter()
             .flat_map(|e| expand_on_gid(e).into_iter().filter(|e| e.gid() == gid))
+            .filter(|e| self.temporal_filter(e))
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use fapolicy_rules::{Decision, Object, Permission, Subject};
+
+    const TEST_PATH: &str = "/bin/bash";
+
+    fn events() -> EventDB {
+        let t = Event {
+            rule_id: 0,
+            dec: Decision::AllowAudit,
+            perm: Permission::Any,
+            uid: 0,
+            gid: vec![0],
+            pid: 0,
+            subj: Subject::from_exe(TEST_PATH),
+            obj: Object::from_path(TEST_PATH),
+            when: None,
+        };
+        let events = (0..=5)
+            .map(|i| Event {
+                rule_id: i,
+                when: Some(DateTime::from_utc(
+                    NaiveDateTime::from_timestamp(i as i64, 0),
+                    Utc,
+                )),
+                ..t.clone()
+            })
+            .collect();
+        EventDB::from(events)
+    }
+
+    #[test]
+    fn temporal_filtering() {
+        let e = events();
+        let all = e.len();
+        let mut log = PyEventLog {
+            rs: e,
+            rs_trust: Default::default(),
+            start: Some(0),
+            stop: Some(5),
+        };
+        assert_eq!(all, log.by_subject(TEST_PATH).len());
+
+        log.begin(Some(1));
+        assert_eq!(all - 1, log.by_subject(TEST_PATH).len());
+
+        log.until(Some(4));
+        assert_eq!(all - 2, log.by_subject(TEST_PATH).len());
+
+        log.until(Some(3));
+        assert_eq!(all - 3, log.by_subject(TEST_PATH).len());
+
+        log.begin(Some(2));
+        assert_eq!(all - 4, log.by_subject(TEST_PATH).len());
+
+        log.begin(None);
+        log.until(None);
+        assert_eq!(all, log.by_subject(TEST_PATH).len());
+
+        log.until(Some(3));
+        assert_eq!(all - 2, log.by_subject(TEST_PATH).len());
     }
 }
 
