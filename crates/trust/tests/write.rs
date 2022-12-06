bdbba20dc949673e4938732b412b19b938a4025c
John Wass <wassj@ctc.com>
2022-12-06 17:21:19 -0500

    headers

--- crates/trust/tests/write.rs
+++ crates/trust/tests/write.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright Concurrent Technologies Corporation 2021
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use fapolicy_trust::db::{Rec, DB};
 use fapolicy_trust::source::TrustSource::DFile;
 use fapolicy_trust::write;
