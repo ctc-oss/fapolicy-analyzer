ctc-oss/fapolicy-analyzer

commit 594bee047611c58c794a2677c926f87da2f29862

Fix service active check (#852)
The active check now uses the unit's loaded status to determine validity.

This check may not be entirely accurate in general, but it is an
improvement and works in the cases we are concerned about.

Also: The cargo-vendor-filterer crate recently released a new version
that requires Rust 1.64. This PR pins our install in the Makefile to the
previous v0.5.8 for el8 compat.

Also: Commits the lock file which was missed back in #845

Closes #851

crates/pyo3/src/daemon.rs
@@ -71,8 +71,10 @@ impl PyHandle {
     }
 
     /// returns true if the unit exists, false otherwise
-    pub fn is_valid(&self) -> bool {
-        self.rs.active().is_ok()
+    pub fn is_valid(&self) -> PyResult<bool> {
+        self.rs
+            .valid()
+            .map_err(|e| PyRuntimeError::new_err(format!("{:?}", e)))
     }
 
     #[args(timeout = 15)]

.copr/Makefile
@@ -36,7 +36,8 @@ dnf-fc:
 dnf-el:
 	dnf install -y rpmdevtools
 	dnf install -y rust-toolset
-	cargo install cargo-vendor-filterer
+	# v0.5.9 requires rust 1.64
+	cargo install cargo-vendor-filterer@0.5.8
 
 ifeq ($(OS_ID),rhel)
 # we only need to vendor rust and python on rhel

crates/daemon/src/svc.rs
@@ -122,10 +122,6 @@ impl Handle {
         self.state().map(|state| matches!(state, State::Active))
     }
 
-    pub fn valid(&self) -> bool {
-        self.state().is_ok()
-    }
-
     pub fn state(&self) -> Result<State, Error> {
         use dbus::arg::messageitem::Props;
         use dbus::ffidisp::Connection;
@@ -151,6 +147,29 @@ impl Handle {
         } else {
             Err(ServiceCheckFailure(
                 "DBUS unit active check failed".to_string(),
+            ))
+        }
+    }
+
+    pub fn valid(&self) -> Result<bool, Error> {
+        use dbus::arg::messageitem::Props;
+        use dbus::ffidisp::Connection;
+
+        let c = Connection::new_system()?;
+        let p = Props::new(
+            &c,
+            "org.freedesktop.systemd1",
+            // todo;; the path name may need to be fetched dynamically via a Message
+            format!("/org/freedesktop/systemd1/unit/{}_2eservice", self.name),
+            "org.freedesktop.systemd1.Unit",
+            5000,
+        );
+
+        if let MessageItem::Str(state) = p.get("LoadState")? {
+            Ok(matches!(state.as_str(), "loaded"))
+        } else {
+            Err(ServiceCheckFailure(
+                "DBUS unit valid check failed".to_string(),
             ))
         }
     }

fapolicy_analyzer/tests/test_fapd_manager.py
@@ -155,7 +155,7 @@ def test_initial_daemon_status(fapdManager, mocker):
 
 def test_initial_daemon_status_w_exception(mocker):
     mocker.patch(
-        "fapolicy_analyzer.ui.fapd_manager.Handle.is_active",
+        "fapolicy_analyzer.ui.fapd_manager.Handle.is_valid",
         side_effect=IOError()
     )
 

commit 3e88b86bc3e7f8de3618ec85c6ff9d7d027677d4

Fix service valid check (#861)
Fix a regression from 594bee0 by flattening the service valid check to
false on dbus error.

The original contract of the valid check was that it was infallible in
order to provide a simple way to check service status. The change was an
unintentional artifact left from debugging while improving the service
status checks.

Closes #860

crates/pyo3/src/daemon.rs
@@ -70,11 +70,9 @@ impl PyHandle {
             .map_err(|e| PyRuntimeError::new_err(format!("{:?}", e)))
     }
 
-    /// returns true if the unit exists, false otherwise
-    pub fn is_valid(&self) -> PyResult<bool> {
-        self.rs
-            .valid()
-            .map_err(|e| PyRuntimeError::new_err(format!("{:?}", e)))
+    /// returns true if the unit is valid, false otherwise
+    pub fn is_valid(&self) -> bool {
+        self.rs.valid().unwrap_or(false)
     }
 
     #[args(timeout = 15)]