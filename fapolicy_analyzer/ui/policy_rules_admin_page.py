9623df14df9b8c225f25e6b5810f8e78299b30d1
elliot <elliotbicker@gmail.com>
2023-05-02 13:35:49 -0700

    debug

--- fapolicy_analyzer/ui/policy_rules_admin_page.py
+++ fapolicy_analyzer/ui/policy_rules_admin_page.py
@@ -448,7 +448,8 @@ class PolicyRulesAdminPage(UIConnectedWidget, UIPage):
             self.__events_loading = False
             self.__log = eventsState.log
             utc = datetime.datetime.now(datetime.timezone.utc)
-            tzdelta = int(time.localtime().tm_gmtoff)
+            tzdelta = 0# int(time.localtime().tm_gmtoff)
+            print(time.time() + tzdelta - self._time_delay)
             if self._time_delay < 0:
                 self.__log.begin(int(time.time()) + tzdelta - 3600)
             else:
