93d0319c8ed386a064ac0eddb4ad8432a080589a
elliot <elliotbicker@gmail.com>
2023-05-03 08:35:11 -0700

    rust debug

--- fapolicy_analyzer/ui/policy_rules_admin_page.py
+++ fapolicy_analyzer/ui/policy_rules_admin_page.py
@@ -449,11 +449,11 @@ class PolicyRulesAdminPage(UIConnectedWidget, UIPage):
             self.__log = eventsState.log
             utc = datetime.datetime.now(datetime.timezone.utc)
             tzdelta = int(time.localtime().tm_gmtoff)
-            print(time.time() + tzdelta - self._time_delay)
+           # print(time.time() + tzdelta - self._time_delay)
             if self._time_delay < 0:
-                self.__log.begin(int(time.time()) - tzdelta - 3600)
+                self.__log.begin(int(utc) + tzdelta - 3600)
             else:
-                self.__log.begin(int(time.time()) - tzdelta - self._time_delay)
+                self.__log.begin(int(utc) + tzdelta - self._time_delay)
             exec_primary_data_func()
 
         if userState.error and not userState.loading and self.__users_loading:
