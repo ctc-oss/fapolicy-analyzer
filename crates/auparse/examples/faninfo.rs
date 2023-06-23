1cd1a83704f1df3bcef0c9743272963f2568a9b2
John Wass <wassj@ctc.com>
2023-06-22 23:42:19 -0400

    wip

--- crates/auparse/examples/faninfo.rs
+++ crates/auparse/examples/faninfo.rs
@@ -19,6 +19,14 @@ pub struct FanEvent {
     pub when: Option<DateTime<Utc>>,
 }
 
+//type=PROCTITLE msg=audit(06/23/2023 03:37:44.624:8583) : proctitle=-bash
+// type=PATH msg=audit(06/23/2023 03:37:44.624:8583) : item=0 name=/tmp/exec inode=2311 dev=00:2f mode=file,755 ouid=vagrant ogid=vagrant rdev=00:00 obj=unconfined_u:object_r:user_tmp_t:s0 nametype=NORMAL cap_fp=none cap_fi=none cap_fe=0 cap_fver=0 cap_frootid=0
+// type=CWD msg=audit(06/23/2023 03:37:44.624:8583) : cwd=/home/vagrant
+// type=SYSCALL msg=audit(06/23/2023 03:37:44.624:8583) : arch=x86_64 syscall=execve success=no exit=EPERM(Operation not permitted) a0=0x56294d1799d0 a1=0x56294d1bb300 a2=0x56294d181820 a3=0x8 items=1 ppid=118132 pid=118216 auid=vagrant uid=vagrant gid=vagrant euid=vagrant suid=vagrant fsuid=vagrant egid=vagrant sgid=vagrant fsgid=vagrant tty=pts0 ses=81 comm=bash exe=/usr/bin/bash subj=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023 key=(null)
+// type=FANOTIFY msg=audit(06/23/2023 03:37:44.624:8583) : resp=deny fan_type=rule_info fan_info=2 subj_trust=unknown obj_trust=no
+
+//////////////////////////
+
 // type=PROCTITLE msg=audit(06/13/2023 13:27:11.267:1224) : proctitle=tee /tmp/deny/foo
 // type=PATH msg=audit(06/13/2023 13:27:11.267:1224) : item=1 name=/tmp/deny/foo inode=56 dev=00:2f mode=file,644 ouid=vagrant ogid=vagrant rdev=00:00 obj=unconfined_u:object_r:user_tmp_t:s0 nametype=NORMAL cap_fp=none cap_fi=none cap_fe=0 cap_fver=0 cap_frootid=0
 // type=PATH msg=audit(06/13/2023 13:27:11.267:1224) : item=0 name=/tmp/deny/ inode=54 dev=00:2f mode=dir,755 ouid=vagrant ogid=vagrant rdev=00:00 obj=unconfined_u:object_r:user_tmp_t:s0 nametype=PARENT cap_fp=none cap_fi=none cap_fe=0 cap_fver=0 cap_frootid=0
