a56af71adc69be1f8bfa79899a7e69843879fd86
John Wass <wassj@ctc.com>
2023-06-24 10:13:52 -0400

    an appending build-info script

--- scripts/build-info.py
+++ scripts/build-info.py
@@ -14,35 +14,37 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import json
+import os
 import subprocess
 import argparse
-from os import path
 
-parse = argparse.ArgumentParser()
-parse.add_argument("path")
-args = parse.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--output", type=str, default="fapolicy_analyzer/resources/build-info.json", help="Path to write output")
+parser.add_argument("--os", action='store_true', help="Add OS info")
+parser.add_argument("--git", action='store_true', help="Add Git info")
+parser.add_argument("--time", action='store_true', help="Add build time")
+parser.add_argument("--overwrite", action='store_true', help="Overwrite build-info (default append)")
 
+args = parser.parse_args()
 
-os_info = subprocess.getoutput(["uname -nr"])
-git_info = subprocess.getoutput(["git log -n 1"])
-time_info = subprocess.getoutput("date")
+if not os.path.exists(args.output) and args.overwrite:
+    print(f"build-info.json not found at {args.output}")
+    exit(1)
 
+data = {}
+if not args.overwrite and os.path.exists(args.output):
+    with open(args.output, "r") as f:
+        data = json.load(f)
 
-if path.isfile(args.path):
+if args.os:
+    data["os_info"] = subprocess.getoutput("uname -nr")
+if args.git:
+    data["git_info"] = subprocess.getoutput("git rev-list HEAD -n1")
+if args.time:
+    data["time_info"] = subprocess.getoutput("date")
 
-    try:
-        data = {
-            "os_info" : os_info,
-            "git_info" : git_info,
-            "time_info" : time_info,
-        }
-        print(data)
-        with open(args.path, "w") as f:
-            json.dump(data, f, indent=4)
-
-
-    except Exception:
-        print(f"Could not open {args.path}")
-
+with open(args.output, "w") as f:
+    json.dump(data, f, indent=4)
+    print(data)
 
 
