c2174c33ae89c5eb850fed9018bb2c3a90ac0845
John Wass <wassj@ctc.com>
2023-06-28 01:05:07 -0400

    headers

--- crates/auparse/sys/src/event.rs
+++ crates/auparse/sys/src/event.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright Concurrent Technologies Corporation 2023
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use crate::error::Error;
 use crate::util::{audit_get_int, audit_get_str};
 use crate::{auparse_get_time, auparse_get_type, auparse_next_event, auparse_state_t};
