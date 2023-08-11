8dbe4dc0560feb96761bc79421f3e188eeacf8ed
John Wass <wassj@ctc.com>
2023-08-11 19:46:48 -0400

    bindings

--- crates/pyo3/src/config.rs
+++ crates/pyo3/src/config.rs
@@ -19,6 +19,27 @@ fn config_file_path() -> PyResult<String> {
         .map_err(|e| PyRuntimeError::new_err(format!("{:?}", e)))
 }
 
+/// log dir
+/// xdg or /var/log/fapolicy-analyzer/
+#[pyfunction]
+fn app_log_dir() -> String {
+    cfg::log_dir()
+}
+
+/// data dir (for autosave things)
+/// xdg or /var/lib/fapolicy-analyzer/
+#[pyfunction]
+fn app_data_dir() -> String {
+    cfg::data_dir()
+}
+
+/// config dir
+/// xdg or /etc/fapolicy-analyzer/
+#[pyfunction]
+fn app_config_dir() -> String {
+    cfg::config_dir()
+}
+
 pub fn init_module(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(config_file_path, m)?)?;
     Ok(())
