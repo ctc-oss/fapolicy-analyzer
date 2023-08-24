bbfb2bafbcb3a7d9c1e88c21ad88e0590227ba48
John Wass <wassj@ctc.com>
2023-08-24 01:47:14 -0400

    parsing

--- crates/daemon/src/conf/config.rs
+++ crates/daemon/src/conf/config.rs
@@ -1,21 +1,4 @@
-#[derive(Clone)]
-pub enum WatchFs {
-    Ok(String),
-    Bad(String),
-}
-
-impl WatchFs {
-    fn defaults() -> Vec<Self> {
-        vec![
-            "ext2", "ext3", "ext4", "tmpfs", "xfs", "vfat", "iso9660", "btrfs",
-        ]
-        .iter()
-        .map(|s| WatchFs::Ok(s.to_string()))
-        .collect()
-    }
-}
-
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum TrustBackend {
     Rpm,
     File,
@@ -28,7 +11,7 @@ impl TrustBackend {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum IntegritySource {
     None,
     Size,
@@ -37,22 +20,65 @@ pub enum IntegritySource {
 
 #[derive(Clone)]
 pub struct Config {
-    permissive: bool,
-    nice_val: usize,
-    q_size: usize,
-    uid: String,
-    gid: String,
-    do_stat_report: bool,
-    detailed_report: bool,
-    db_max_size: usize,
-    subj_cache_size: usize,
-    obj_cache_size: usize,
-    watch_fs: Vec<WatchFs>,
-    trust: Vec<TrustBackend>,
-    integrity: IntegritySource,
-    syslog_format: String,
-    rpm_sha256_only: bool,
-    allow_filesystem_mark: bool,
+    pub permissive: bool,
+    pub nice_val: usize,
+    pub q_size: usize,
+    pub uid: String,
+    pub gid: String,
+    pub do_stat_report: bool,
+    pub detailed_report: bool,
+    pub db_max_size: usize,
+    pub subj_cache_size: usize,
+    pub obj_cache_size: usize,
+    pub watch_fs: Vec<String>,
+    pub trust: Vec<TrustBackend>,
+    pub integrity: IntegritySource,
+    pub syslog_format: Vec<String>,
+    pub rpm_sha256_only: bool,
+    pub allow_filesystem_mark: bool,
+}
+
+#[derive(Debug)]
+pub(crate) enum ConfigToken {
+    Permissive(bool),
+    NiceVal(usize),
+    QSize(usize),
+    UID(String),
+    GID(String),
+    DoStatReport(bool),
+    DetailedReport(bool),
+    DbMaxSize(usize),
+    SubjCacheSize(usize),
+    ObjCacheSize(usize),
+    WatchFs(Vec<String>),
+    Trust(Vec<TrustBackend>),
+    Integrity(IntegritySource),
+    SyslogFormat(Vec<String>),
+    RpmSha256Only(bool),
+    AllowFsMark(bool),
+}
+
+impl ConfigToken {
+    pub fn apply(self, conf: &mut Config) {
+        match self {
+            ConfigToken::Permissive(v) => conf.permissive = v,
+            ConfigToken::NiceVal(v) => conf.nice_val = v,
+            ConfigToken::QSize(v) => conf.q_size = v,
+            ConfigToken::UID(v) => conf.uid = v,
+            ConfigToken::GID(v) => conf.gid = v,
+            ConfigToken::DoStatReport(v) => conf.do_stat_report = v,
+            ConfigToken::DetailedReport(v) => conf.detailed_report = v,
+            ConfigToken::DbMaxSize(v) => conf.db_max_size = v,
+            ConfigToken::SubjCacheSize(v) => conf.subj_cache_size = v,
+            ConfigToken::ObjCacheSize(v) => conf.obj_cache_size = v,
+            ConfigToken::WatchFs(v) => conf.watch_fs = v,
+            ConfigToken::Trust(v) => conf.trust = v,
+            ConfigToken::Integrity(v) => conf.integrity = v,
+            ConfigToken::SyslogFormat(v) => conf.syslog_format = v,
+            ConfigToken::RpmSha256Only(v) => conf.rpm_sha256_only = v,
+            ConfigToken::AllowFsMark(v) => conf.allow_filesystem_mark = v,
+        }
+    }
 }
 
 impl Default for Config {
@@ -68,10 +94,16 @@ impl Default for Config {
             db_max_size: 50,
             subj_cache_size: 1549,
             obj_cache_size: 8191,
-            watch_fs: WatchFs::defaults(),
+            watch_fs: "ext2,ext3,ext4,tmpfs,xfs,vfat,iso9660,btrfs"
+                .split(",")
+                .map(|s| s.to_string())
+                .collect(),
             trust: TrustBackend::defaults(),
             integrity: IntegritySource::None,
-            syslog_format: "rule,dec,perm,auid,pid,exe,:,path,ftype,trust".to_string(),
+            syslog_format: "rule,dec,perm,auid,pid,exe,:,path,ftype,trust"
+                .split(",")
+                .map(|s| s.to_string())
+                .collect(),
             rpm_sha256_only: false,
             allow_filesystem_mark: false,
         }
--- crates/daemon/src/conf/parse.rs
+++ crates/daemon/src/conf/parse.rs
@@ -1,28 +1,192 @@
-use crate::conf::error::Error;
-use crate::Config;
-use fapolicy_rules::parser::error::RuleParseError::{ExpectedPermAssignment, ExpectedPermTag};
-use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::alphanumeric1;
-use nom::combinator::opt;
-use nom::sequence::tuple;
-use nom::{IResult, Parser};
+use nom::character::complete::{alphanumeric1, digit1};
+use nom::multi::separated_list0;
+use nom::IResult;
 
-type Res<'a, R> = IResult<&'a str, R, Error>;
+use crate::conf::config::{ConfigToken, IntegritySource, TrustBackend};
+use crate::conf::error::Error;
+use crate::conf::error::Error::{
+    ExpectedBool, ExpectedIntegritySource, ExpectedNumber, ExpectedString, ExpectedStringList,
+    General, Unexpected, UnknownTrustBackend,
+};
 
-fn parse_bool(i: &str) -> Res<bool> {
-    match tag("0").parse(i) {
-        Ok((i, _)) => Ok((i, false)),
+fn parse_bool<'a>(i: &'a str) -> Result<bool, Error> {
+    match digit1::<&'a str, nom::error::Error<&'a str>>(i) {
+        Ok(("", v)) if v == "1" => Ok(true),
+        Ok(("", v)) if v == "0" => Ok(false),
+        Ok((_, _)) => Err(Unexpected(i.to_string())),
+        Err(_) => Err(General),
     }
 }
 
+fn parse_number<'a>(i: &'a str) -> Result<usize, Error> {
+    match digit1::<&'a str, nom::error::Error<&'a str>>(i) {
+        Ok(("", v)) => v.parse().map_err(|_| ExpectedNumber),
+        Ok((_, _)) => Err(Unexpected(i.to_string())),
+        Err(_) => Err(General),
+    }
+}
+
+fn nom_str(i: &str) -> IResult<&str, &str> {
+    alphanumeric1(i)
+}
+
+fn nom_num(i: &str) -> IResult<&str, usize, Error> {
+    digit1(i).map(|(r, v)| (r, v.parse().unwrap()))
+}
+
+fn parse_string(i: &str) -> Result<String, Error> {
+    match nom_str(i) {
+        Ok(("", v)) => v.parse().map_err(|_| ExpectedString),
+        Ok((_, _)) => Err(Unexpected(i.to_string())),
+        Err(e) => Err(General),
+    }
+}
+
+fn parse_trust_backend(i: &str) -> Result<Vec<TrustBackend>, Error> {
+    use TrustBackend::*;
+
+    let mut res = vec![];
+    for s in parse_string_list(i)? {
+        match s.as_str() {
+            "rpm" => res.push(Rpm),
+            "file" => res.push(File),
+            "deb" => res.push(Deb),
+            unk => return Err(UnknownTrustBackend(unk.to_string())),
+        }
+    }
+    Ok(res)
+}
+
+fn parse_integrity(i: &str) -> Result<IntegritySource, Error> {
+    match parse_string(i).as_deref() {
+        Ok("none") => Ok(IntegritySource::None),
+        Ok("hash") => Ok(IntegritySource::Hash),
+        Ok("size") => Ok(IntegritySource::Size),
+        Ok(_) => Err(ExpectedIntegritySource),
+        Err(e) => Err(e.clone()),
+    }
+}
+
+fn parse_string_list(i: &str) -> Result<Vec<String>, Error> {
+    match nom_str_list(i) {
+        Ok(("", l)) => Ok(l.into_iter().map(|s| s.to_string()).collect()),
+        Ok((_, _)) => Err(Unexpected(i.to_string())),
+        Err(e) => Err(General),
+    }
+}
+
+// fn parse_id<'a>(i: &'a str) -> Result<String, Error> {
+//     alt((digit1, nom_str))
+// }
+
+fn nom_str_list(i: &str) -> IResult<&str, Vec<&str>> {
+    separated_list0(tag(","), nom_str)(i)
+}
+
+fn p2<'a>(i: &'a str) -> Result<ConfigToken, Error> {
+    use ConfigToken::*;
+    match i.split_once("=") {
+        None | Some(("", _)) | Some((_, "")) => Err(Error::MalformedConfig),
+        Some((lhs, rhs)) => match lhs {
+            // bools
+            "permissive" => parse_bool(rhs).map(Permissive).map_err(|_| ExpectedBool),
+            "do_stat_report" => parse_bool(rhs).map(DoStatReport).map_err(|_| ExpectedBool),
+            "detailed_report" => parse_bool(rhs)
+                .map(DetailedReport)
+                .map_err(|_| ExpectedBool),
+            "rpm_sha256_only" => parse_bool(rhs).map(RpmSha256Only).map_err(|_| ExpectedBool),
+            "allow_filesystem_mark" => parse_bool(rhs).map(AllowFsMark).map_err(|_| ExpectedBool),
+            // numbers
+            "nice_val" => parse_number(rhs).map(NiceVal).map_err(|_| ExpectedNumber),
+            "q_size" => parse_number(rhs).map(QSize).map_err(|_| ExpectedNumber),
+            "db_max_size" => parse_number(rhs).map(DbMaxSize).map_err(|_| ExpectedNumber),
+            "subj_cache_size" => parse_number(rhs)
+                .map(SubjCacheSize)
+                .map_err(|_| ExpectedNumber),
+            "obj_cache_size" => parse_number(rhs)
+                .map(ObjCacheSize)
+                .map_err(|_| ExpectedNumber),
+            // strings
+            "watch_fs" => parse_string_list(rhs)
+                .map(WatchFs)
+                .map_err(|_| ExpectedStringList),
+            "syslog_format" => parse_string_list(rhs)
+                .map(SyslogFormat)
+                .map_err(|_| ExpectedStringList),
+            // typed
+            "trust" => parse_trust_backend(rhs).map(Trust),
+            "integrity" => parse_trust_backend(rhs).map(Trust),
+            unsupported => Err(Error::InvalidLhs(unsupported.to_string())),
+        },
+    }
+}
+
+//         tag("uid"),
+//         tag("gid"),
+//         tag("watch_fs"),
+//         tag("trust"),
+//         tag("integrity"),
+//         tag("syslog_format"),
+//     ))(i) {
+//         Ok((r, v)) => Ok(v),
+//         Err(e) =>
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
-    use crate::conf::parse::parse_bool;
     use assert_matches::assert_matches;
+
+    use crate::conf::config::ConfigToken::*;
+    use crate::conf::error::Error;
+    use crate::conf::parse::{p2, parse_bool};
 
     #[test]
     fn test_parse_bool() {
-        assert_matches!(parse_bool("0"), Ok((_, false)))
+        assert_matches!(parse_bool("0"), Ok(false));
+        assert_matches!(parse_bool("1"), Ok(true));
+        assert_matches!(parse_bool("2"), Err(_));
+        assert_matches!(parse_bool("x"), Err(_));
+        assert_matches!(parse_bool(""), Err(_));
+    }
+
+    #[test]
+    fn test_p2_malformed_config() {
+        assert_matches!(p2(""), Err(Error::MalformedConfig));
+        assert_matches!(p2("foo"), Err(Error::MalformedConfig));
+        assert_matches!(p2("foo="), Err(Error::MalformedConfig));
+        assert_matches!(p2("=foo"), Err(Error::MalformedConfig));
+    }
+
+    #[test]
+    fn test_p2_permissive() {
+        assert_matches!(p2("permissive=0"), Ok(Permissive(false)));
+        assert_matches!(p2("permissive=1"), Ok(Permissive(true)));
+        assert_matches!(p2("permissive=foo"), Err(Error::ExpectedBool));
+    }
+
+    #[test]
+    fn test_p2_nice_val() {
+        assert_matches!(p2("nice_val=0"), Ok(NiceVal(0)));
+        assert_matches!(p2("nice_val=14"), Ok(NiceVal(14)));
+        assert_matches!(p2("nice_val=foo"), Err(Error::ExpectedNumber));
+    }
+
+    #[test]
+    fn test_p2_watch_fs() {
+        // assert_matches!(p2("watch_fs="), Ok(WatchFs(v)) if v.is_empty());
+        assert_matches!(p2("watch_fs=ext2"), Ok(WatchFs(v)) if v.len() == 1);
+        assert_matches!(
+            p2("watch_fs=ext2,ext3"),
+            Ok(WatchFs(v)) if v.len() == 2
+        );
+    }
+
+    #[test]
+    fn test_p2_trust_sources() {
+        // assert_matches!(p2("trust="), Ok(Trust(v)) if v.is_empty());
+        assert_matches!(p2("trust=rpm"), Ok(Trust(v)) if v.len() == 1);
+        assert_matches!(p2("trust=rpm,file"), Ok(Trust(v)) if v.len() == 2);
     }
 }
