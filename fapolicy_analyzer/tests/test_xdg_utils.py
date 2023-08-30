6c629a638d9467d190b8179d4ec3c24aa0f1b5e1
tparchambault <development@TPArchambault.com>
2023-08-30 00:28:49 -0400

    fixed xdg feature unit tests of xdg_utils.py

--- fapolicy_analyzer/tests/test_xdg_utils.py
+++ fapolicy_analyzer/tests/test_xdg_utils.py
@@ -15,6 +15,8 @@
 
 import os
 
+import pytest
+
 from fapolicy_analyzer.util.xdg_utils import (
     app_config_dir_prefix,
     app_data_dir_prefix,
@@ -22,7 +24,43 @@ from fapolicy_analyzer.util.xdg_utils import (
 )
 
 
-def test_app_state_dir_prefix():
+@pytest.fixture
+def feature_xdg(mocker):
+    _home = os.path.expanduser("~")
+    fapa = "fapolicy-analyzer"
+    mocker.patch(
+        "fapolicy_analyzer.util.xdg_utils.app_log_dir",
+        return_value=os.path.join(_home, ".local", "state", fapa),
+    )
+    mocker.patch(
+        "fapolicy_analyzer.util.xdg_utils.app_data_dir",
+        return_value=os.path.join(_home, ".local", "share", fapa),
+    )
+    mocker.patch(
+        "fapolicy_analyzer.util.xdg_utils.app_config_dir",
+        return_value=os.path.join(_home, ".config", fapa),
+    )
+
+
+@pytest.fixture
+def feature_no_xdg(mocker):
+    fapa = "fapolicy-analyzer"
+    mocker.patch(
+        "fapolicy_analyzer.util.xdg_utils.app_log_dir",
+        return_value=os.path.join("/var", "log", fapa),
+    )
+    mocker.patch(
+        "fapolicy_analyzer.util.xdg_utils.app_data_dir",
+        return_value=os.path.join("/var", "lib", fapa),
+    )
+    mocker.patch(
+        "fapolicy_analyzer.util.xdg_utils.app_config_dir",
+        return_value=os.path.join("/etc", fapa),
+    )
+
+
+@pytest.mark.usefixtures("feature_xdg")
+def test_app_state_dir_prefix_w_xdg():
     strBasename = "Arbitrary.txt"
     strHomeDir = os.environ.get("HOME")
     strStateDefaultDir = strHomeDir + "/.local/share/"
@@ -31,7 +69,8 @@ def test_app_state_dir_prefix():
     assert app_state_dir_prefix(strBasename) == strExpected
 
 
-def test_app_data_dir_prefix():
+@pytest.mark.usefixtures("feature_xdg")
+def test_app_data_dir_prefix_w_xdg():
     strBasename = "Arbitrary.txt"
     strHomeDir = os.environ.get("HOME")
     strDataDefaultDir = strHomeDir + "/.local/state/"
@@ -40,10 +79,43 @@ def test_app_data_dir_prefix():
     assert app_data_dir_prefix(strBasename) == strExpected
 
 
-def test_app_config_dir_prefix():
+@pytest.mark.usefixtures("feature_xdg")
+def test_app_config_dir_prefix_w_xdg():
     strBasename = "Arbitrary.txt"
     strHomeDir = os.environ.get("HOME")
     strConfigDefaultDir = strHomeDir + "/.config/"
     strExpected = strConfigDefaultDir + "fapolicy-analyzer/" + strBasename
 
     assert app_config_dir_prefix(strBasename) == strExpected
+
+
+@pytest.mark.usefixtures("feature_no_xdg")
+def test_app_state_dir_prefix_wo_xdg(mocker):
+    mockMakeDirs = mocker.patch("fapolicy_analyzer.util.xdg_utils.os.makedirs")
+    strBasename = "Arbitrary.txt"
+    strStateDefaultDir = os.path.join("/var", "lib")
+    strExpected = strStateDefaultDir + "/fapolicy-analyzer/" + strBasename
+
+    assert app_state_dir_prefix(strBasename) == strExpected
+    mockMakeDirs.assert_called()
+
+
+@pytest.mark.usefixtures("feature_no_xdg")
+def test_app_data_dir_prefix_wo_xdg(mocker):
+    mockMakeDirs = mocker.patch("fapolicy_analyzer.util.xdg_utils.os.makedirs")
+    strBasename = "Arbitrary.txt"
+    strDataDefaultDir = os.path.join("/var", "log")
+    strExpected = strDataDefaultDir + "/fapolicy-analyzer/" + strBasename
+
+    assert app_data_dir_prefix(strBasename) == strExpected
+    mockMakeDirs.assert_called()
+
+
+@pytest.mark.usefixtures("feature_no_xdg")
+def test_app_config_dir_prefix_wo_xdg(mocker):
+    mockMakeDirs = mocker.patch("fapolicy_analyzer.util.xdg_utils.os.makedirs")
+    strBasename = "Arbitrary.txt"
+    strExpected = "/etc/fapolicy-analyzer/" + strBasename
+
+    assert app_config_dir_prefix(strBasename) == strExpected
+    mockMakeDirs.assert_called()
