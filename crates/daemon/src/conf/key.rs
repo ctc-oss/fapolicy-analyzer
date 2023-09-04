2503533a34e4ab8efd49513d7b3e93610446fe0c
John Wass <wassj@ctc.com>
2023-09-04 08:13:47 -0400

    dates

--- crates/daemon/src/conf/key.rs
+++ crates/daemon/src/conf/key.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright Concurrent Technologies Corporation 2021
+ * Copyright Concurrent Technologies Corporation 2023
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
