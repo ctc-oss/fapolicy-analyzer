d3aea6a98087c6793b822e4ad9c30746b9f0296e
John Wass <wassj@ctc.com>
2023-09-05 09:30:44 -0400

    header

--- crates/daemon/src/conf/ops.rs
+++ crates/daemon/src/conf/ops.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright Concurrent Technologies Corporation 2023
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use crate::conf::error::Error;
 use crate::conf::{load, DB};
 
