bb50ff0d71d85f4cd45ef0059af41ba4cba035ad
John Wass <wassj@ctc.com>
2023-12-11 07:46:28 -0500

    tests to ensure escape parsing is handled

---
+++ crates/analyzer/tests/test_escaped_logs.rs
@@ -0,0 +1,64 @@
+use fapolicy_analyzer::events::parse::parse_event;
+use std::error::Error;
+
+// todo;; test space char
+const ESCAPE_CHARS: &str = "\"'`$\\!()|";
+
+#[test]
+fn test_object_escape() -> Result<(), Box<dyn Error>> {
+    // not escaped
+    for c in ESCAPE_CHARS.chars() {
+        let c = c.to_string();
+        let log = format!("rule=1 dec=deny perm=open uid=1 gid=0 pid=7 exe=xyz : path=/foo{c}bar ftype=application/x-executable");
+        let (_, e) = parse_event(&log).unwrap();
+        assert_eq!(e.obj.path().unwrap(), format!("/foo{c}bar"));
+    }
+    // escaped
+    for c in ESCAPE_CHARS.chars() {
+        let log = format!("rule=1 dec=deny perm=open uid=1 gid=0 pid=7 exe=xyz : path=/foo\\{c}bar ftype=application/x-executable");
+        let (_, e) = parse_event(&log).unwrap();
+        assert_eq!(e.obj.path().unwrap(), format!("/foo\\{c}bar"));
+    }
+    Ok(())
+}
+
+#[test]
+fn test_subj_exe_escape() -> Result<(), Box<dyn Error>> {
+    // not escaped
+    for c in ESCAPE_CHARS.chars() {
+        let c = c.to_string();
+        let log = format!("rule=1 dec=deny perm=open uid=1 gid=0 pid=7 exe=foo{c}bar : path=/usr/bin/vi ftype=application/x-executable");
+        let (_, e) = parse_event(&log).unwrap();
+        assert_eq!(e.subj.exe().unwrap(), format!("foo{c}bar"));
+    }
+    // escaped
+    for c in ESCAPE_CHARS.chars() {
+        let log = format!("rule=1 dec=deny perm=open uid=1 gid=0 pid=7 exe=foo\\{c}bar : path=/usr/bin/vi ftype=application/x-executable");
+        let (_, e) = parse_event(&log).unwrap();
+        assert_eq!(e.subj.exe().unwrap(), format!("foo\\{c}bar"));
+    }
+    Ok(())
+}
+
+#[test]
+fn test_subj_command_escape() -> Result<(), Box<dyn Error>> {
+    // not escaped
+    for c in ESCAPE_CHARS.chars() {
+        let c = c.to_string();
+        let log = format!("rule=1 dec=deny perm=open uid=1 gid=0 pid=7 comm=foo{c}bar : path=/usr/bin/vi ftype=application/x-executable");
+        let (_, e) = parse_event(&log).unwrap();
+        assert_eq!(e.subj.comm().unwrap(), format!("foo{c}bar"));
+    }
+    // escaped
+    for c in ESCAPE_CHARS.chars() {
+        let log = format!("rule=1 dec=deny perm=open uid=1 gid=0 pid=7 comm=foo\\{c}bar : path=/usr/bin/vi ftype=application/x-executable");
+        let (_, e) = parse_event(&log).unwrap();
+        assert_eq!(e.subj.comm().unwrap(), format!("foo\\{c}bar"));
+    }
+    Ok(())
+}
+
+#[test]
+fn num_of_escapes() {
+    assert_eq!(ESCAPE_CHARS.len(), 9)
+}
