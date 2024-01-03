eeddf46ebee87289a60740f776de658124e5858c
John Wass <wassj@ctc.com>
2024-01-03 16:58:14 -0500

    added test for empty file

--- crates/util/tests/sha256_test.rs
+++ crates/util/tests/sha256_test.rs
@@ -11,9 +11,17 @@ use std::fs::File;
 use std::io::BufReader;
 
 #[test]
-fn test_hashme() {
+fn test_non_empty() {
     let expected = "047bc85db1001a7c98c13f594178d339efc60e3b099af5d27a65498ddc808f55";
     let f = File::open("tests/data/hashme.txt").expect("failed to open file");
+    let actual = sha256_digest(BufReader::new(&f)).expect("failed to hash file");
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_empty() {
+    let expected = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    let f = File::open("tests/data/mt.txt").expect("failed to open file");
     let actual = sha256_digest(BufReader::new(&f)).expect("failed to hash file");
     assert_eq!(actual, expected);
 }
