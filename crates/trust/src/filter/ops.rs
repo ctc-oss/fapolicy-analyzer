e0cfc0de4def623e822340e7f1a2266df685a441
John Wass <wassj@ctc.com>
2024-06-03 14:43:22 -0400

    headers

--- crates/trust/src/filter/ops.rs
+++ crates/trust/src/filter/ops.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright Concurrent Technologies Corporation 2023
+ * Copyright Concurrent Technologies Corporation 2024
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
