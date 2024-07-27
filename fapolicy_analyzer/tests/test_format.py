509dd85a7f8e1707306559d74044ed2a3dae4dd7
TPArchambault <development@TPArchambault.com>
2024-07-27 11:42:26 -0400

    added check to see if kwargs dict was populated for Py 3.13

--- fapolicy_analyzer/tests/test_format.py
+++ fapolicy_analyzer/tests/test_format.py
@@ -44,15 +44,28 @@ def test_f():
 def test_f_none():
     assert f(None) is None
 
-@pytest.mark.parametrize("version, f_in, eval_out",[((3,9), "foo", "insert is foo"),
+@pytest.mark.parametrize("py_ver, f_in, eval_out",[((3,9), "foo", "insert is foo"),
                                                   ((3,12), "bar", "insert is bar"),
                                                   ((3,13), "baz", "insert is baz")])
-def test_f_supported_py_vers(version, f_in, eval_out, mocker):
+def test_f_supported_py_vers(py_ver, f_in, eval_out, mocker):
     insert = f_in
     mockEval = mocker.patch('builtins.eval', return_value=f"insert is {insert}")
     mockSysVer = mocker.patch.object(fapolicy_analyzer.util.format, "sys")
-    mockSysVer.version_info = version
+    mockSysVer.version_info = py_ver
 
     assert f(None) is None
     assert f(f"insert is {insert}") == eval_out
-    assert mockEval.called
+
+    # Check if keyword args were in eval()'s call
+    assert mockEval.called_once
+    tupleEvalArgs = mockEval.call_args
+
+    if py_ver < (3, 13):
+        # Py <3.13 only uses positional args, Kwargs dict should be empty
+        assert not tupleEvalArgs.kwargs
+
+    else:
+        # Py 3.13 call uses kw args in our code
+        assert tupleEvalArgs.kwargs
+        assert "locals" in tupleEvalArgs.kwargs
+        assert "globals" in tupleEvalArgs.kwargs
