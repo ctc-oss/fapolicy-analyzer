2cc1c4546e3478d64554ef2d441d8032d2ab9ef6
John Wass <wassj@ctc.com>
2024-11-29 20:24:34 -0500

    wip

--- crates/daemon/tests/parse_stats.rs
+++ crates/daemon/tests/parse_stats.rs
@@ -6,16 +6,3 @@ fn parse_stat_rec() {
     let rec = stats::parse("tests/data/state.0").expect("failed to parse state");
     println!("{:?}", rec);
 }
-
-// #[test]
-// fn watch_stat_changes() {
-//     let db = Arc::new(Mutex::new(stats::Db::new()));
-//     let f = tempfile::NamedTempFile::new().expect("failed to create temp file");
-//     let rx =
-//         stats::read(&f.path().display().to_string(), db.clone()).expect("failed to read stats");
-//
-//     println!("=================");
-//     for _ in rx.iter() {
-//         println!("boom")
-//     }
-// }
