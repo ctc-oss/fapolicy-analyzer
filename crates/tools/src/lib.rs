346ef1d0ae9eeadcdd30c9d843b7c2e57448af6f
John Wass <wassj@ctc.com>
2025-03-01 08:14:07 -0500

    fix human panic

--- crates/tools/src/lib.rs
+++ crates/tools/src/lib.rs
@@ -16,16 +16,10 @@
 use human_panic::{setup_panic, Metadata};
 
 pub fn setup_human_panic() {
-    setup_panic!(
-        Metadata::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
-            .authors("CTC-OSS and contributors")
-            .homepage("https://github.com/ctc-oss/fapolicy-analyzer")
-            .support(
-                [
-                    "- Open a GitHub issue on the fapolicy-analyzer project page",
-                    "- Open a Red Hat Bugzilla issue for the fapolicy-analyzer package"
-                ]
-                .join("\n")
-            )
-    );
+    setup_panic!(Metadata {
+        version: env!("CARGO_PKG_VERSION").into(),
+        name: env!("CARGO_PKG_NAME").into(),
+        authors: "CTC-OSS and contributors".into(),
+        homepage: "https://github.com/ctc-oss/fapolicy-analyzer".into()
+    });
 }
