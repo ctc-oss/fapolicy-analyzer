c63220e58c9f49e6bcb7e5c97ea226ad3abfe86e
John Wass <wassj@ctc.com>
2025-07-21 00:19:29 -0400

    Preserve daemon state on deploy
    The daemon should not start after a deploy if it was not previously running.
    
    Closes #836

--- crates/pyo3/src/daemon.rs
+++ crates/pyo3/src/daemon.rs
@@ -122,12 +122,17 @@ fn fapolicyd_version() -> Option<String> {
 
 pub(crate) fn deploy(system: &PySystem) -> PyResult<()> {
     let handle = PyHandle::default();
-    handle
+    let was_active = handle.is_active()?;
+    let plan = handle
         .stop()
         .and_then(|_| handle.wait_until_inactive(15))
-        .and_then(|_| system.deploy_only())
-        .and_then(|_| handle.start())
-        .and_then(|_| handle.wait_until_active(15))
+        .and_then(|_| system.deploy_only());
+    match was_active {
+        false => plan,
+        true => plan
+            .and_then(|_| handle.start())
+            .and_then(|_| handle.wait_until_active(15)),
+    }
 }
 
 #[pyfunction]
