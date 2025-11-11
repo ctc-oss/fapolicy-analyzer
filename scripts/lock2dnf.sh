6b71907707b7a8374bda6189b9406d046d2eee39
John Wass <wassj@ctc.com>
2025-11-11 14:36:20 -0500

    add lock2dnf script

---
+++ scripts/lock2dnf.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# install all available packages for a Cargo.lock
+# generate a temporary spec file and use builddep to install
+
+if [ -z "$1" ]; then
+  echo "usage: lock2dnf <path-to-cargo-toml>"
+  exit 1
+fi
+
+tmpfile=$(mktemp)
+trap 'rm -f "$tmpfile"' EXIT
+
+echo "Name: fapolicy-analyzer-builddeps" > "${tmpfile}"
+echo "Summary: Cargo2RPM Build Dependencies" | tee -a "${tmpfile}"
+echo "Version: 0.0.0" | tee -a "${tmpfile}"
+echo "License: None" | tee -a "${tmpfile}"
+echo "Release: 1" | tee -a "${tmpfile}"
+cargo2rpm --path "$1" buildrequires --with-check --all-features | sed 's/^/BuildRequires: /' | tee -a "${tmpfile}"
+echo "%description" | tee -a "${tmpfile}"
+dnf builddep --spec "${tmpfile}"
